85883273038d8cd36f70cd285433af3a405592c7
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-02-15 15:09:31 +0800

    Update zhou_csaw.R

--- zhou_csaw.R
+++ zhou_csaw.R
@@ -1,1 +1,11 @@
 
+require(csaw)
+
+param <- readParam(minq=10)
+data <- windowCounts(bam.files, ext=110, width=10, param=param)
+
+ require(DESeq2)
+
+binned <- windowCounts(bam.files, bin=TRUE, width=10000, param=param)
+
+regionCounts(bam.files, regions, ext=100, param=readParam())
