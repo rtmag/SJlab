01a08250a64b4f12f4db737a9e04e1cd1426e319
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-03-01 02:03:46 +0800

    Update nicole.sh

--- nicole.sh
+++ nicole.sh
@@ -6,3 +6,36 @@ awk -F  "\t" \
 '{if($4=="+"){print $1"\t"$2-2000"\t"$2"\t"$5"\t"1000"\t"$4} if($4=="-"){print $1"\t"$3"\t"$3+2000"\t"$5"\t"1000"\t"$4} }' \
 > hg19_tss_2kb.bed
 
+#####
+
+STAR --genomeDir ~/resources/star_index_overhang100/ \
+--readFilesCommand zcat \
+--runThreadN 15 \
+--readFilesIn /home/rtm/SJlab/nicole/bgi/cdts-hk.genomics.cn/F16FTSAPHT0948_HUMgqaE/Clean/LPCX-siC/FCHCYTLBBXX_L8_HKRDHUMgqaEAAARAAPEI-227_1.fq.gz \
+/home/rtm/SJlab/nicole/bgi/cdts-hk.genomics.cn/F16FTSAPHT0948_HUMgqaE/Clean/LPCX-siC/FCHCYTLBBXX_L8_HKRDHUMgqaEAAARAAPEI-227_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate  \
+--outFileNamePrefix /home/rtm/SJlab/nicole/bgi/bam/LPCX-siC_
+
+STAR --genomeDir ~/resources/star_index_overhang100/ \
+--readFilesCommand zcat \
+--runThreadN 15 \
+--readFilesIn /home/rtm/SJlab/nicole/bgi/cdts-hk.genomics.cn/F16FTSAPHT0948_HUMgqaE/Clean/LPCX-siK/FCHCYTLBBXX_L8_HKRDHUMgqaEAABRAAPEI-201_1.fq.gz \
+/home/rtm/SJlab/nicole/bgi/cdts-hk.genomics.cn/F16FTSAPHT0948_HUMgqaE/Clean/LPCX-siK/FCHCYTLBBXX_L8_HKRDHUMgqaEAABRAAPEI-201_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate  \
+--outFileNamePrefix /home/rtm/SJlab/nicole/bgi/bam/LPCX-siK_
+
+STAR --genomeDir ~/resources/star_index_overhang100/ \
+--readFilesCommand zcat \
+--runThreadN 15 \
+--readFilesIn /home/rtm/SJlab/nicole/bgi/cdts-hk.genomics.cn/F16FTSAPHT0948_HUMgqaE/Clean/TIP60-siC/FCHCYTLBBXX_L8_HKRDHUMgqaEAACRAAPEI-202_1.fq.gz \
+/home/rtm/SJlab/nicole/bgi/cdts-hk.genomics.cn/F16FTSAPHT0948_HUMgqaE/Clean/TIP60-siC/FCHCYTLBBXX_L8_HKRDHUMgqaEAACRAAPEI-202_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate  \
+--outFileNamePrefix /home/rtm/SJlab/nicole/bgi/bam/TIP60-siC_
+
+STAR --genomeDir ~/resources/star_index_overhang100/ \
+--readFilesCommand zcat \
+--runThreadN 15 \
+--readFilesIn /home/rtm/SJlab/nicole/bgi/cdts-hk.genomics.cn/F16FTSAPHT0948_HUMgqaE/Clean/TIP60-siK/FCHCYTLBBXX_L8_HKRDHUMgqaEAADRAAPEI-205_1.fq.gz \
+/home/rtm/SJlab/nicole/bgi/cdts-hk.genomics.cn/F16FTSAPHT0948_HUMgqaE/Clean/TIP60-siK/FCHCYTLBBXX_L8_HKRDHUMgqaEAADRAAPEI-205_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate  \
+--outFileNamePrefix /home/rtm/SJlab/nicole/bgi/bam/TIP60-siK_
