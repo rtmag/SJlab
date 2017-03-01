90dfa7fdbf390a4eab8de6bcd9cd012b794bb037
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-03-01 15:24:51 +0800

    Update Nicole_rna_seq.R

--- Nicole_rna_seq.R
+++ Nicole_rna_seq.R
@@ -1,1 +1,20 @@
+library(Rsubread)
+library(DESeq2)
 
+############# Feature Count; Meta-Gene by exons #############
+r1=""
+r2="/home/rtm/SJlab/nicole/bgi/bam"
+
+data<-featureCounts(c(
+"10A_siC.bam",
+"10A_siT.bam"),
+annot.ext="~/tblab/resources/gencode.v19.annotation.gtf_withproteinids",
+isGTFAnnotationFile=TRUE,
+minMQS=10,
+strandSpecific=0,
+isPairedEnd=TRUE,
+#PE_orientation="rf",
+autosort=TRUE,
+nthreads=3,
+GTF.attrType="gene_name"
+)
