52d2815d8e9bf879b4097b59b7e4aef6c130549f
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-03-10 16:36:21 +0800

    Create deepa_repenrich.R

---
+++ deepa_repenrich.R
@@ -0,0 +1,10 @@
+sic_class=read.table("HCT_siC_class_fraction_counts.txt",sep="\t",header=F)
+sik_class=read.table("HCT_siK_class_fraction_counts.txt",sep="\t",header=F)
+
+sic_family=read.table("HCT_siC_family_fraction_counts.txt",sep="\t",header=F)
+sik_family=read.table("HCT_siK_family_fraction_counts.txt",sep="\t",header=F)
+
+sic_all=read.table("HCT_siC_fraction_counts.txt",sep="\t",header=F)
+sik_all=read.table("HCT_siK_fraction_counts.txt",sep="\t",header=F)
+
+
