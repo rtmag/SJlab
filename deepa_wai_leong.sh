4a21fac56da46ff45fcc778d24a3817cb5b17375
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-04-17 14:50:20 +0800

    Update deepa_wai_leong.sh

--- deepa_wai_leong.sh
+++ deepa_wai_leong.sh
@@ -12,7 +12,7 @@ mkdir star_hg38_gencode26_overhang149
 
 /home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --genomeDir /home/roberto/references/star_hg38_gencode26_overhang149 \
 --readFilesCommand zcat \
---runThreadN 15 \
+--runThreadN 35 \
 --readFilesIn /home/roberto/deepa/tam_wai_leong/Sample_RHC8260/RHC8260-GCCAAT_S2_L001_R1_001.fastq.gz \
 /home/roberto/deepa/tam_wai_leong/Sample_RHC8268/RHC8260-GCCAAT_S2_L001_R2_001.fastq.gz \
 --outSAMtype BAM SortedByCoordinate  \
@@ -20,7 +20,7 @@ mkdir star_hg38_gencode26_overhang149
 
 /home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --genomeDir /home/roberto/references/star_hg38_gencode26_overhang149 \
 --readFilesCommand zcat \
---runThreadN 15 \
+--runThreadN 35 \
 --readFilesIn /home/roberto/deepa/tam_wai_leong/Sample_RHC8268/RHC8268-GGCTAC_S10_L001_R1_001.fastq.gz \
 /home/roberto/deepa/tam_wai_leong/Sample_RHC8268/RHC8268-GGCTAC_S10_L001_R2_001.fastq.gz \
 --outSAMtype BAM SortedByCoordinate  \
@@ -28,7 +28,7 @@ mkdir star_hg38_gencode26_overhang149
 
 /home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --genomeDir /home/roberto/references/star_hg38_gencode26_overhang149 \
 --readFilesCommand zcat \
---runThreadN 15 \
+--runThreadN 35 \
 --readFilesIn /home/roberto/deepa/tam_wai_leong/Sample_RHC8261/RHC8261-CTTGTA_S3_L001_R1_001.fastq.gz \
 /home/roberto/deepa/tam_wai_leong/Sample_RHC8261/RHC8261-CTTGTA_S3_L001_R2_001.fastq.gz \
 --outSAMtype BAM SortedByCoordinate  \
@@ -36,7 +36,7 @@ mkdir star_hg38_gencode26_overhang149
 
 /home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --genomeDir /home/roberto/references/star_hg38_gencode26_overhang149 \
 --readFilesCommand zcat \
---runThreadN 15 \
+--runThreadN 35 \
 --readFilesIn /home/roberto/deepa/tam_wai_leong/Sample_RHC8269/RHC8269-GTGGCC_S11_L001_R1_001.fastq.gz \
 /home/roberto/deepa/tam_wai_leong/Sample_RHC8269/RHC8269-GTGGCC_S11_L001_R2_001.fastq.gz \
 --outSAMtype BAM SortedByCoordinate  \
@@ -44,7 +44,7 @@ mkdir star_hg38_gencode26_overhang149
 
 /home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --genomeDir /home/roberto/references/star_hg38_gencode26_overhang149 \
 --readFilesCommand zcat \
---runThreadN 15 \
+--runThreadN 35 \
 --readFilesIn /home/roberto/deepa/tam_wai_leong/Sample_RHC8271/RHC8271-TTAGGC_S13_L001_R1_001.fastq.gz \
 /home/roberto/deepa/tam_wai_leong/Sample_RHC8271/RHC8271-TTAGGC_S13_L001_R2_001.fastq.gz \
 --outSAMtype BAM SortedByCoordinate  \
@@ -52,7 +52,7 @@ mkdir star_hg38_gencode26_overhang149
 
 /home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --genomeDir /home/roberto/references/star_hg38_gencode26_overhang149 \
 --readFilesCommand zcat \
---runThreadN 15 \
+--runThreadN 35 \
 --readFilesIn /home/roberto/deepa/tam_wai_leong/Sample_RHC8273/RHC8273-CGTACG_S15_L001_R1_001.fastq.gz \
 /home/roberto/deepa/tam_wai_leong/Sample_RHC8273/RHC8273-CGTACG_S15_L001_R2_001.fastq.gz \
 --outSAMtype BAM SortedByCoordinate  \
@@ -61,7 +61,7 @@ mkdir star_hg38_gencode26_overhang149
 
 /home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --genomeDir /home/roberto/references/star_hg38_gencode26_overhang149 \
 --readFilesCommand zcat \
---runThreadN 15 \
+--runThreadN 35 \
 --readFilesIn /home/roberto/deepa/tam_wai_leong/Sample_RHC8265/RHC8265-CGATGT_S7_L001_R1_001.fastq.gz \
 /home/roberto/deepa/tam_wai_leong/Sample_RHC8265/RHC8265-CGATGT_S7_L001_R2_001.fastq.gz \
 --outSAMtype BAM SortedByCoordinate  \
@@ -69,7 +69,7 @@ mkdir star_hg38_gencode26_overhang149
 
 /home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --genomeDir /home/roberto/references/star_hg38_gencode26_overhang149 \
 --readFilesCommand zcat \
---runThreadN 15 \
+--runThreadN 35 \
 --readFilesIn /home/roberto/deepa/tam_wai_leong/Sample_RHC8263/RHC8263-AGTCAA_S5_L001_R1_001.fastq.gz \
 /home/roberto/deepa/tam_wai_leong/Sample_RHC8263/RHC8263-AGTCAA_S5_L001_R2_001.fastq.gz \
 --outSAMtype BAM SortedByCoordinate  \
