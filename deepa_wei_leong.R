9aa508e65796418790abd61599dd34e1b432fa10
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-04-24 15:51:14 +0800

    Update deepa_wei_leong.R

--- deepa_wei_leong.R
+++ deepa_wei_leong.R
@@ -1,1 +1,23 @@
 
+library(Rsubread)
+library(DESeq2)
+library(graphics)
+
+####
+
+
+
+data<-featureCounts(c(
+"/home/rtm/SJlab/nicole/bgi/bam/feb2015_207_Aligned.sortedByCoord.out.bam",
+"/home/rtm/SJlab/nicole/bgi/bam/feb2015_208_Aligned.sortedByCoord.out.bam"
+),
+annot.ext="/home/rtm/resources/hg19_genes.gtf",
+isGTFAnnotationFile=TRUE,
+minMQS=10,
+strandSpecific=0,
+isPairedEnd=TRUE,
+#PE_orientation="rf",
+autosort=TRUE,
+nthreads=15,
+GTF.attrType="gene_name"
+)
