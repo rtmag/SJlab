95c7b700a263ab15b2532ba32ea64fcd3fb06ee2
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-04-27 04:45:53 +0800

    Update deepa__RepEnrich_wai_leong.sh

--- deepa__RepEnrich_wai_leong.sh
+++ deepa__RepEnrich_wai_leong.sh
@@ -26,26 +26,26 @@ trim_galore --illumina --clip_R1 12 -o /home/roberto/deepa/tam_wai_leong/trimmed
 ##
 bowtie /home/roberto/references/hg19_bowtie/hg19 -p 60 -t -m 1 -S \
 --max /home/roberto/deepa/tam_wai_leong/RepEnrich/P360_tumor_multimap.fastq \
--1 <( zcat /home/roberto/deepa/tam_wai_leong/Sample_RHC8265/RHC8265-CGATGT_S7_L001_R1_001.fastq.gz ) \
--2 <( zcat /home/roberto/deepa/tam_wai_leong/Sample_RHC8265/RHC8265-CGATGT_S7_L001_R2_001.fastq.gz ) \
+-1 <( zcat /home/roberto/deepa/tam_wai_leong/trimmed/RHC8265-CGATGT_S7_L001_R1_001_trimmed.fastq.gz ) \
+-2 <( zcat /home/roberto/deepa/tam_wai_leong/trimmed/RHC8265-CGATGT_S7_L001_R2_001_trimmed.fastq.gz ) \
 /home/roberto/deepa/tam_wai_leong/RepEnrich/P360_tumor_uniq.sam
 
 bowtie /home/roberto/references/hg19_bowtie/hg19 -p 60 -t -m 1 -S \
 --max /home/roberto/deepa/tam_wai_leong/RepEnrich/P1446_tumor_multimap.fastq \
--1 <( zcat /home/roberto/deepa/tam_wai_leong/Sample_RHC8269/RHC8269-GTGGCC_S11_L001_R1_001.fastq.gz ) \
--2 <( zcat /home/roberto/deepa/tam_wai_leong/Sample_RHC8269/RHC8269-GTGGCC_S11_L001_R2_001.fastq.gz ) \
+-1 <( zcat /home/roberto/deepa/tam_wai_leong/trimmed/RHC8269-GTGGCC_S11_L001_R1_001_trimmed.fastq.gz ) \
+-2 <( zcat /home/roberto/deepa/tam_wai_leong/trimmed/RHC8269-GTGGCC_S11_L001_R2_001_trimmed.fastq.gz ) \
 /home/roberto/deepa/tam_wai_leong/RepEnrich/P1446_tumor_uniq.sam
 
 bowtie /home/roberto/references/hg19_bowtie/hg19 -p 60 -t -m 1 -S \
 --max /home/roberto/deepa/tam_wai_leong/RepEnrich/P1456_tumor_multimap.fastq \
--1 <( zcat /home/roberto/deepa/tam_wai_leong/Sample_RHC8271/RHC8271-TTAGGC_S13_L001_R1_001.fastq.gz ) \
--2 <( zcat /home/roberto/deepa/tam_wai_leong/Sample_RHC8271/RHC8271-TTAGGC_S13_L001_R2_001.fastq.gz ) \
+-1 <( zcat /home/roberto/deepa/tam_wai_leong/trimmed/RHC8271-TTAGGC_S13_L001_R1_001_trimmed.fastq.gz ) \
+-2 <( zcat /home/roberto/deepa/tam_wai_leong/trimmed/RHC8271-TTAGGC_S13_L001_R2_001_trimmed.fastq.gz ) \
 /home/roberto/deepa/tam_wai_leong/RepEnrich/P1456_tumor_uniq.sam
 
 bowtie /home/roberto/references/hg19_bowtie/hg19 -p 60 -t -m 1 -S \
 --max /home/roberto/deepa/tam_wai_leong/RepEnrich/P520_tumor_multimap.fastq \
--1 <( zcat /home/roberto/deepa/tam_wai_leong/Sample_RHC8263/RHC8263-AGTCAA_S5_L001_R1_001.fastq.gz ) \
--2 <( zcat /home/roberto/deepa/tam_wai_leong/Sample_RHC8263/RHC8263-AGTCAA_S5_L001_R2_001.fastq.gz ) \
+-1 <( zcat /home/roberto/deepa/tam_wai_leong/trimmed/RHC8263-AGTCAA_S5_L001_R1_001_trimmed.fastq.gz ) \
+-2 <( zcat /home/roberto/deepa/tam_wai_leong/trimmed/RHC8263-AGTCAA_S5_L001_R2_001_trimmed.fastq.gz ) \
 /home/roberto/deepa/tam_wai_leong/RepEnrich/P520_tumor_uniq.sam
 
 
