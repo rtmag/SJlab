b9048904c7cb99701f9ff30d5f894605d5db29fe
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-04-27 03:39:17 +0800

    Create deepa_repenrich.sh

---
+++ deepa_repenrich.sh
@@ -0,0 +1,14 @@
+trim_galore --illumina --clip_R1 12 -o /home/roberto/deepa/tam_wai_leong/trimmed \
+/home/roberto/deepa/novogene/HCT116_siC_1.fq.gz \
+/home/roberto/deepa/novogene/HCT116_siC_2.fq.gz  \
+/home/roberto/deepa/novogene/HCT116_siC_DMSO_1.fq.gz  \
+/home/roberto/deepa/novogene/HCT116_siC_DMSO_2.fq.gz  \
+/home/roberto/deepa/novogene/HCT116_siC_JQ1_1.fq.gz  \
+/home/roberto/deepa/novogene/HCT116_siC_JQ1_2.fq.gz  \
+/home/roberto/deepa/novogene/HCT116_siIRF7_DMSO_1.fq.gz  \
+/home/roberto/deepa/novogene/HCT116_siIRF7_DMSO_2.fq.gz  \
+/home/roberto/deepa/novogene/HCT116_siIRF7_JQ1_1.fq.gz  \
+/home/roberto/deepa/novogene/HCT116_siIRF7_JQ1_2.fq.gz  \
+/home/roberto/deepa/novogene/HCT116_siK_1.fq.gz  \
+/home/roberto/deepa/novogene/HCT116_siK_2.fq.gz  
+
