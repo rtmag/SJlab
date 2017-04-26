e347c8eea9e9ffff380b66bd0403744d78430885
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-04-27 04:29:18 +0800

    Update deepa_repenrich.sh

--- deepa_repenrich.sh
+++ deepa_repenrich.sh
@@ -1,4 +1,4 @@
-trim_galore --illumina --clip_R1 12 -o /home/roberto/deepa/tam_wai_leong/trimmed \
+trim_galore --illumina --clip_R1 12 -o /home/roberto/deepa/novogene/trimmed \
 /home/roberto/deepa/novogene/HCT116_siC_1.fq.gz \
 /home/roberto/deepa/novogene/HCT116_siC_2.fq.gz  \
 /home/roberto/deepa/novogene/HCT116_siC_DMSO_1.fq.gz  \
@@ -10,5 +10,14 @@ trim_galore --illumina --clip_R1 12 -o /home/roberto/deepa/tam_wai_leong/trimmed
 /home/roberto/deepa/novogene/HCT116_siIRF7_JQ1_1.fq.gz  \
 /home/roberto/deepa/novogene/HCT116_siIRF7_JQ1_2.fq.gz  \
 /home/roberto/deepa/novogene/HCT116_siK_1.fq.gz  \
-/home/roberto/deepa/novogene/HCT116_siK_2.fq.gz  
+/home/roberto/deepa/novogene/HCT116_siK_2.fq.gz  \
+/home/roberto/deepa/novogene/HCT_DMSO_HWN2YCCXX_L2_1.fq.gz \
+/home/roberto/deepa/novogene/HCT_DMSO_HWN2YCCXX_L2_2.fq.gz  \
+/home/roberto/deepa/novogene/HCT_JQ1_HWN2YCCXX_L5_1.fq.gz  \
+/home/roberto/deepa/novogene/HCT_JQ1_HWN2YCCXX_L5_2.fq.gz  \
+/home/roberto/deepa/novogene/HCT_siControl_HWN2YCCXX_L5_1.fq.gz  \
+/home/roberto/deepa/novogene/HCT_siControl_HWN2YCCXX_L5_2.fq.gz  \
+/home/roberto/deepa/novogene/HCT_siK_HVNYLCCXX_L2_1.fq.gz  \
+/home/roberto/deepa/novogene/HCT_siK_HVNYLCCXX_L2_2.fq.gz  \
+
 
