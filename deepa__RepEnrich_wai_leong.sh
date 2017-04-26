68555b87778d264faefd45bf67921e463d88bdd3
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-04-26 21:05:30 +0800

    Update deepa__RepEnrich_wai_leong.sh

--- deepa__RepEnrich_wai_leong.sh
+++ deepa__RepEnrich_wai_leong.sh
@@ -1,5 +1,19 @@
 python ~/myPrograms/RepEnrich/RepEnrich_setup.py hg19_repeatmasker_clean.txt hg19.fa RepEnrich_hg19
 
+##
+~/myPrograms/FastQC/fastqc /home/roberto/deepa/tam_wai_leong/Sample_RHC8265/RHC8265-CGATGT_S7_L001_R1_001.fastq.gz \
+/home/roberto/deepa/tam_wai_leong/Sample_RHC8265/RHC8265-CGATGT_S7_L001_R2_001.fastq.gz \
+/home/roberto/deepa/tam_wai_leong/Sample_RHC8269/RHC8269-GTGGCC_S11_L001_R1_001.fastq.gz \
+/home/roberto/deepa/tam_wai_leong/Sample_RHC8269/RHC8269-GTGGCC_S11_L001_R2_001.fastq.gz \
+/home/roberto/deepa/tam_wai_leong/Sample_RHC8271/RHC8271-TTAGGC_S13_L001_R1_001.fastq.gz \
+/home/roberto/deepa/tam_wai_leong/Sample_RHC8271/RHC8271-TTAGGC_S13_L001_R2_001.fastq.gz \
+/home/roberto/deepa/tam_wai_leong/Sample_RHC8263/RHC8263-AGTCAA_S5_L001_R1_001.fastq.gz \
+/home/roberto/deepa/tam_wai_leong/Sample_RHC8263/RHC8263-AGTCAA_S5_L001_R2_001.fastq.gz \
+-o fastqc
+
+
+
+##
 bowtie /home/roberto/references/hg19_bowtie/hg19 -p 60 -t -m 1 -S \
 --max /home/roberto/deepa/tam_wai_leong/RepEnrich/P360_tumor_multimap.fastq \
 -1 <( zcat /home/roberto/deepa/tam_wai_leong/Sample_RHC8265/RHC8265-CGATGT_S7_L001_R1_001.fastq.gz ) \
