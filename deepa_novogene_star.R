51f886697afed869012092a9ad89df2029a55bb2
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-05-17 18:42:48 +0800

    Update deepa_novogene_star.R

--- deepa_novogene_star.R
+++ deepa_novogene_star.R
@@ -1,12 +1,16 @@
+library(Rsubread)
+
 data<-featureCounts(c(
-"/home/roberto/deepa/tam_wai_leong/bam/P019WKF_normal_Aligned.sortedByCoord.out.bam",
-"/home/roberto/deepa/tam_wai_leong/bam/P1446_normal_Aligned.sortedByCoord.out.bam",
-"/home/roberto/deepa/tam_wai_leong/bam/P019WKF_tumor_Aligned.sortedByCoord.out.bam",
-"/home/roberto/deepa/tam_wai_leong/bam/P1446_tumor_Aligned.sortedByCoord.out.bam",
-"/home/roberto/deepa/tam_wai_leong/bam/P1456_tumor_Aligned.sortedByCoord.out.bam",
-"/home/roberto/deepa/tam_wai_leong/bam/P1463_tumor_Aligned.sortedByCoord.out.bam",
-"/home/roberto/deepa/tam_wai_leong/bam/P360_tumor_Aligned.sortedByCoord.out.bam",
-"/home/roberto/deepa/tam_wai_leong/bam/P520_tumor_Aligned.sortedByCoord.out.bam"
+"/home/roberto/deepa/novogene/STAR/HCT116_siC_Aligned.sortedByCoord.out.bam",
+"/home/roberto/deepa/novogene/STAR/HCT116_siC_DMSO_Aligned.sortedByCoord.out.bam",
+"/home/roberto/deepa/novogene/STAR/HCT116_siC_JQ1_Aligned.sortedByCoord.out.bam",
+"/home/roberto/deepa/novogene/STAR/HCT116_siIRF7_DMSO_Aligned.sortedByCoord.out.bam",
+"/home/roberto/deepa/novogene/STAR/HCT116_siIRF7_JQ1_Aligned.sortedByCoord.out.bam",
+"/home/roberto/deepa/novogene/STAR/HCT116_siK_Aligned.sortedByCoord.out.bam",
+"/home/roberto/deepa/novogene/STAR/HCT_DMSO_HWN2YCCXX_L2_Aligned.sortedByCoord.out.bam",
+"/home/roberto/deepa/novogene/STAR/HCT_JQ1_HWN2YCCXX_L5_Aligned.sortedByCoord.out.bam",
+"/home/roberto/deepa/novogene/STAR/HCT_siControl_HWN2YCCXX_L5_Aligned.sortedByCoord.out.bam",
+"/home/roberto/deepa/novogene/STAR/HCT_siK_HVNYLCCXX_L2_Aligned.sortedByCoord.out.bam"
 ),
 annot.ext="/home/roberto/references/gencode.v26.annotation.gtf",
 isGTFAnnotationFile=TRUE,
@@ -18,3 +22,24 @@ autosort=TRUE,
 nthreads=15,
 GTF.attrType="gene_name"
 )
+
+dat=data[[1]]
+colnames(dat)<-gsub("X.home.roberto.deepa.novogene.STAR.","",colnames(dat))
+colnames(dat)<-gsub("_Aligned.sortedByCoord.out.bam","",colnames(dat))
+saveRDS(dat,"novogene_counts.rds")
+
+###
+countData=dat
+design<-data.frame(experiment=colnames(countData[,c(1,6,9,10)]), batch = c("r1","r1","r2","r2"),
+                                            condition = c("siC","siC", "siK","siK") )
+
+dLRT <- DESeqDataSetFromMatrix(countData = countData[,c(1,6,9,10)], colData = design, design = ~ batch + condiion )
+dLRT <- DESeq(dLRT, test="LRT",full= ~ batch + condition , reduced=~ batch )
+dDif_res <- results(dLRT,contrast=c("condition","siC","siK"))
+
+export=dDif_res[which(dDif_res$padj<0.05 & abs(dDif_res$log2FoldChange)>.5),]
+write.csv(export,"LPCX-siC_TIP60-siC_r1r2_DEG_revised_log2FC-0.5_padj-0.05.csv")
+write.csv(dDif_res,"LPCX-siC_TIP60-siC_r1r2_DEG_revised_all.csv")
+
+###
+
