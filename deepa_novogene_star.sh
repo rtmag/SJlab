4e664d76b8b6cb8a96dcc947c077c41d39ae5e70
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-05-17 12:35:50 +0800

    Create deepa_novogene_star.sh

---
+++ deepa_novogene_star.sh
@@ -0,0 +1,82 @@
+
+/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --genomeDir /home/roberto/references/star_hg38_gencode26_overhang149 \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--readFilesIn /home/roberto/deepa/novogene/HCT116_siC_1.fq.gz \
+/home/roberto/deepa/novogene/HCT116_siC_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate  \
+--outFileNamePrefix /home/roberto/deepa/novogene/STAR/HCT116_siC_
+
+/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --genomeDir /home/roberto/references/star_hg38_gencode26_overhang149 \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--readFilesIn /home/roberto/deepa/novogene/HCT116_siC_DMSO_1.fq.gz \
+/home/roberto/deepa/novogene/HCT116_siC_DMSO_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate  \
+--outFileNamePrefix /home/roberto/deepa/novogene/STAR/HCT116_siC_DMSO_
+
+/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --genomeDir /home/roberto/references/star_hg38_gencode26_overhang149 \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--readFilesIn /home/roberto/deepa/novogene/HCT116_siC_JQ1_1.fq.gz \
+/home/roberto/deepa/novogene/HCT116_siC_JQ1_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate  \
+--outFileNamePrefix /home/roberto/deepa/novogene/STAR/HCT116_siC_JQ1_
+
+/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --genomeDir /home/roberto/references/star_hg38_gencode26_overhang149 \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--readFilesIn /home/roberto/deepa/novogene/HCT116_siIRF7_DMSO_1.fq.gz \
+/home/roberto/deepa/novogene/HCT116_siIRF7_DMSO_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate  \
+--outFileNamePrefix /home/roberto/deepa/novogene/STAR/HCT116_siIRF7_DMSO_
+
+/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --genomeDir /home/roberto/references/star_hg38_gencode26_overhang149 \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--readFilesIn /home/roberto/deepa/novogene/HCT116_siIRF7_JQ1_1.fq.gz \
+/home/roberto/deepa/novogene/HCT116_siIRF7_JQ1_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate  \
+--outFileNamePrefix /home/roberto/deepa/novogene/STAR/HCT116_siIRF7_JQ1_
+
+/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --genomeDir /home/roberto/references/star_hg38_gencode26_overhang149 \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--readFilesIn /home/roberto/deepa/novogene/HCT116_siK_1.fq.gz \
+/home/roberto/deepa/novogene/HCT116_siK_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate  \
+--outFileNamePrefix /home/roberto/deepa/novogene/STAR/HCT116_siK_
+
+
+/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --genomeDir /home/roberto/references/star_hg38_gencode26_overhang149 \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--readFilesIn /home/roberto/deepa/novogene/HCT_DMSO_HWN2YCCXX_L2_1.fq.gz \
+/home/roberto/deepa/novogene/HCT_DMSO_HWN2YCCXX_L2_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate  \
+--outFileNamePrefix /home/roberto/deepa/novogene/STAR/HCT_DMSO_HWN2YCCXX_L2_
+
+
+/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --genomeDir /home/roberto/references/star_hg38_gencode26_overhang149 \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--readFilesIn /home/roberto/deepa/novogene/HCT_JQ1_HWN2YCCXX_L5_1.fq.gz \
+/home/roberto/deepa/novogene/HCT_JQ1_HWN2YCCXX_L5_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate  \
+--outFileNamePrefix /home/roberto/deepa/novogene/STAR/HCT_JQ1_HWN2YCCXX_L5_
+##
+/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --genomeDir /home/roberto/references/star_hg38_gencode26_overhang149 \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--readFilesIn /home/roberto/deepa/novogene/HCT_siControl_HWN2YCCXX_L5_1.fq.gz \
+/home/roberto/deepa/novogene/HCT_siControl_HWN2YCCXX_L5_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate  \
+--outFileNamePrefix /home/roberto/deepa/novogene/STAR/HCT_siControl_HWN2YCCXX_L5_
+
+/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --genomeDir /home/roberto/references/star_hg38_gencode26_overhang149 \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--readFilesIn /home/roberto/deepa/novogene/HCT_siK_HVNYLCCXX_L2_1.fq.gz \
+/home/roberto/deepa/novogene/HCT_siK_HVNYLCCXX_L2_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate  \
+--outFileNamePrefix /home/roberto/deepa/novogene/STAR/HCT_siK_HVNYLCCXX_L2_
