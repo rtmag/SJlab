5e2cf6a51569609691a0a226be511f8fd3f30b6d
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-09-20 12:20:15 +0800

    Update deepa__RepEnrich_wai_leong.sh

--- deepa__RepEnrich_wai_leong.sh
+++ deepa__RepEnrich_wai_leong.sh
@@ -49,3 +49,53 @@ bowtie /home/roberto/references/hg19_bowtie/hg19 -p 60 -t -m 1 -S \
 /home/roberto/deepa/tam_wai_leong/RepEnrich_trimmed/P520_tumor_uniq.sam
 
 
+#######
+
+samtools view -bS P360_tumor_uniq.sam | samtools sort - P360_tumor_uniq
+samtools index P360_tumor_uniq.bam
+
+samtools view -bS P1446_tumor_uniq.sam | samtools sort - P1446_tumor_uniq
+samtools index P1446_tumor_uniq.bam
+
+samtools view -bS P1456_tumor_uniq.sam | samtools sort - P1456_tumor_uniq
+samtools index P1456_tumor_uniq.bam
+
+samtools view -bS P520_tumor_uniq.sam | samtools sort - P520_tumor_uniq
+samtools index P520_tumor_uniq.bam
+###
+
+python /home/roberto/myPrograms/RepEnrich/RepEnrich.py \
+/home/roberto/references/hg19_repeatmasker_clean.txt \
+/home/roberto/deepa/tam_wai_leong/RepEnrich P360_tumor \
+/home/roberto/references/RepEnrich_hg19/ \
+P360_tumor_multimap_1.fastq \
+--fastqfile2 P360_tumor_multimap_2.fastq \
+P360_tumor_uniq.bam \
+--cpus 30 --pairedend TRUE
+
+python /home/roberto/myPrograms/RepEnrich/RepEnrich.py \
+/home/roberto/references/hg19_repeatmasker_clean.txt \
+/home/roberto/deepa/tam_wai_leong/RepEnrich P1446_tumor \
+/home/roberto/references/RepEnrich_hg19/ \
+P1446_tumor_multimap_1.fastq \
+--fastqfile2 P1446_tumor_multimap_2.fastq \
+P1446_tumor_uniq.bam \
+--cpus 30 --pairedend TRUE
+
+python /home/roberto/myPrograms/RepEnrich/RepEnrich.py \
+/home/roberto/references/hg19_repeatmasker_clean.txt \
+/home/roberto/deepa/tam_wai_leong/RepEnrich P1456_tumor \
+/home/roberto/references/RepEnrich_hg19/ \
+P1456_tumor_multimap_1.fastq \
+--fastqfile2 P1456_tumor_multimap_2.fastq \
+P1456_tumor_uniq.bam \
+--cpus 30 --pairedend TRUE
+
+python /home/roberto/myPrograms/RepEnrich/RepEnrich.py \
+/home/roberto/references/hg19_repeatmasker_clean.txt \
+/home/roberto/deepa/tam_wai_leong/RepEnrich P520_tumor \
+/home/roberto/references/RepEnrich_hg19/ \
+P520_tumor_multimap_1.fastq \
+--fastqfile2 P520_tumor_multimap_2.fastq \
+P520_tumor_uniq.bam \
+--cpus 30 --pairedend TRUE
