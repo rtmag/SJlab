9724dc08ff79d31fafffe1c74de78e82dfbc1f0c
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-09-25 18:26:36 +0800

    Update deepa_wei_leong.R

--- deepa_wei_leong.R
+++ deepa_wei_leong.R
@@ -44,3 +44,39 @@ dLRT_vsd <- varianceStabilizingTransformation(dLRT)
 dLRT_res <- results(dLRT)
 
 plotPCA(dLRT_vsd,ntop=30000,intgroup=c("batch","treatment","vector"))
+
+#######
+
+x=readRDS("wai_leong_counts.rds")
+
+kat5=which(rownames(x)=="KAT5")
+ix=c(which(colnames(x)=="P1446_tumor"),which(colnames(x)=="P1456_tumor"),which(colnames(x)=="P520_tumor"),which(colnames(x)=="P360_tumor"))
+
+x=x[kat5,ix]
+
+P1446=read.table("P1446_tumor_class_fraction_counts.txt")
+P1446=P1446[c(which(P1446[,1]=="LINE"),which(P1446[,1]=="LTR"),which(P1446[,1]=="SINE")),]
+
+P1456=read.table("P1456_tumor_class_fraction_counts.txt")
+P1456=P1456[c(which(P1456[,1]=="LINE"),which(P1456[,1]=="LTR"),which(P1456[,1]=="SINE")),]
+
+P360=read.table("P360_tumor_class_fraction_counts.txt")
+P360=P360[c(which(P360[,1]=="LINE"),which(P360[,1]=="LTR"),which(P360[,1]=="SINE")),]
+
+P520=read.table("P520_tumor_class_fraction_counts.txt")
+P520=P520[c(which(P520[,1]=="LINE"),which(P520[,1]=="LTR"),which(P520[,1]=="SINE")),]
+##
+#1446   1456     520   360
+mat=cbind(P1446[,2],P1456[,2],P360[,2],P520[,2])
+rownames(mat)=P1446[,1]
+colnames(mat)=c("P1446_tumor","P1456_tumor","P520_tumor","P360_tumor")
+
+pdf("leong_pdx_class_kat5.pdf")
+par(mfrow=c(1,1))
+
+barplot(mat,beside=T,col=c("darkturquoise","darkolivegreen2","firebrick3"),border=NA,cex.names=.7)
+legend("topleft",rownames(mat),fill=c("darkturquoise","darkolivegreen2","firebrick3"),border=NA,bty='n')
+    
+barplot(x,border=NA,col="burlywood",cex.names=.7)
+legend("topleft","KAT5",fill=c("burlywood"),border=NA,bty='n')
+dev.off()
