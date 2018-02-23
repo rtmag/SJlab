5bf211d7e1c116c127cb6efc3fdd7a241d5d6e51
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-02-24 03:44:16 +0800

    Update deepa_h3k9me3.sh

--- deepa_h3k9me3.sh
+++ deepa_h3k9me3.sh
@@ -343,4 +343,110 @@ trim_galore --illumina --paired -o /home/roberto/deepa/h3k9me3/fastq_trim/ \
 --outSAMtype BAM SortedByCoordinate \
 --outFileNamePrefix /home/roberto/deepa/h3k9me3/bam/K_k9me3_L004_
 
+######
+#C_h3
+samtools merge -h /home/roberto/deepa/h3k9me3/bam/C_H3_L001_Aligned.sortedByCoord.out.bam /home/roberto/deepa/h3k9me3/bam/C_H3.bam \
+/home/roberto/deepa/h3k9me3/bam/C_H3_L001_Aligned.sortedByCoord.out.bam \
+/home/roberto/deepa/h3k9me3/bam/C_H3_L002_Aligned.sortedByCoord.out.bam \
+/home/roberto/deepa/h3k9me3/bam/C_H3_L003_Aligned.sortedByCoord.out.bam \
+/home/roberto/deepa/h3k9me3/bam/C_H3_L004_Aligned.sortedByCoord.out.bam 
 
+java -jar /home/roberto/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
+I=/home/roberto/deepa/h3k9me3/bam/C_H3.bam \
+O=/home/roberto/deepa/h3k9me3/bam/C_H3_rmdup.bam \
+M=/home/roberto/deepa/h3k9me3/bam/C_H3.mfile
+
+samtools index /home/roberto/deepa/h3k9me3/bam/C_H3_rmdup.bam
+
+bamCoverage -p max -bs 1 --normalizeUsingRPKM -b /home/roberto/deepa/h3k9me3/bam/C_H3_rmdup.bam \
+-o /home/roberto/deepa/h3k9me3/bw/C_H3.bw
+####
+# C_input
+samtools merge -h /home/roberto/deepa/h3k9me3/bam/C_input_L001_Aligned.sortedByCoord.out.bam /home/roberto/deepa/h3k9me3/bam/C_input.bam \
+/home/roberto/deepa/h3k9me3/bam/C_input_L001_Aligned.sortedByCoord.out.bam \
+/home/roberto/deepa/h3k9me3/bam/C_input_L002_Aligned.sortedByCoord.out.bam \
+/home/roberto/deepa/h3k9me3/bam/C_input_L003_Aligned.sortedByCoord.out.bam \
+/home/roberto/deepa/h3k9me3/bam/C_input_L004_Aligned.sortedByCoord.out.bam
+
+java -jar /home/roberto/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
+I=/home/roberto/deepa/h3k9me3/bam/C_input.bam \
+O=/home/roberto/deepa/h3k9me3/bam/C_input_rmdup.bam \
+M=/home/roberto/deepa/h3k9me3/bam/C_input.mfile
+
+samtools index /home/roberto/deepa/h3k9me3/bam/C_input_rmdup.bam
+
+bamCoverage -p max -bs 1 --normalizeUsingRPKM -b /home/roberto/deepa/h3k9me3/bam/C_input_rmdup.bam \
+-o /home/roberto/deepa/h3k9me3/bw/C_input.bw
+####
+# C_k9me3
+samtools merge -h /home/roberto/deepa/h3k9me3/bam/C_k9me3_L001_Aligned.sortedByCoord.out.bam /home/roberto/deepa/h3k9me3/bam/C_k9me3.bam \
+/home/roberto/deepa/h3k9me3/bam/C_k9me3_L001_Aligned.sortedByCoord.out.bam \
+/home/roberto/deepa/h3k9me3/bam/C_k9me3_L002_Aligned.sortedByCoord.out.bam \
+/home/roberto/deepa/h3k9me3/bam/C_k9me3_L003_Aligned.sortedByCoord.out.bam \
+/home/roberto/deepa/h3k9me3/bam/C_k9me3_L004_Aligned.sortedByCoord.out.bam
+
+java -jar /home/roberto/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
+I=/home/roberto/deepa/h3k9me3/bam/C_k9me3.bam \
+O=/home/roberto/deepa/h3k9me3/bam/C_k9me3_rmdup.bam \
+M=/home/roberto/deepa/h3k9me3/bam/C_k9me3.mfile
+
+samtools index /home/roberto/deepa/h3k9me3/bam/C_k9me3_rmdup.bam
+
+bamCoverage -p max -bs 1 --normalizeUsingRPKM -b /home/roberto/deepa/h3k9me3/bam/C_k9me3_rmdup.bam \
+-o /home/roberto/deepa/h3k9me3/bw/C_k9me3.bw
+
+####
+# K_H3
+samtools merge -h /home/roberto/deepa/h3k9me3/bam/K_H3_L001_Aligned.sortedByCoord.out.bam /home/roberto/deepa/h3k9me3/bam/K_H3.bam \
+/home/roberto/deepa/h3k9me3/bam/K_H3_L001_Aligned.sortedByCoord.out.bam \
+/home/roberto/deepa/h3k9me3/bam/K_H3_L002_Aligned.sortedByCoord.out.bam \
+/home/roberto/deepa/h3k9me3/bam/K_H3_L003_Aligned.sortedByCoord.out.bam \
+/home/roberto/deepa/h3k9me3/bam/K_H3_L004_Aligned.sortedByCoord.out.bam 
+
+java -jar /home/roberto/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
+I=/home/roberto/deepa/h3k9me3/bam/K_H3.bam \
+O=/home/roberto/deepa/h3k9me3/bam/K_H3_rmdup.bam \
+M=/home/roberto/deepa/h3k9me3/bam/K_H3.mfile
+
+samtools index /home/roberto/deepa/h3k9me3/bam/K_H3_rmdup.bam
+
+bamCoverage -p max -bs 1 --normalizeUsingRPKM -b /home/roberto/deepa/h3k9me3/bam/K_H3_rmdup.bam \
+-o /home/roberto/deepa/h3k9me3/bw/K_H3.bw
+
+####
+# K_input
+samtools merge -h /home/roberto/deepa/h3k9me3/bam/K_input_L001_Aligned.sortedByCoord.out.bam /home/roberto/deepa/h3k9me3/bam/K_input.bam \
+/home/roberto/deepa/h3k9me3/bam/K_input_L001_Aligned.sortedByCoord.out.bam \
+/home/roberto/deepa/h3k9me3/bam/K_input_L002_Aligned.sortedByCoord.out.bam \
+/home/roberto/deepa/h3k9me3/bam/K_input_L003_Aligned.sortedByCoord.out.bam \
+/home/roberto/deepa/h3k9me3/bam/K_input_L004_Aligned.sortedByCoord.out.bam
+
+java -jar /home/roberto/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
+I=/home/roberto/deepa/h3k9me3/bam/K_input.bam \
+O=/home/roberto/deepa/h3k9me3/bam/K_input_rmdup.bam \
+M=/home/roberto/deepa/h3k9me3/bam/K_input.mfile
+
+samtools index /home/roberto/deepa/h3k9me3/bam/K_input_rmdup.bam
+
+bamCoverage -p max -bs 1 --normalizeUsingRPKM -b /home/roberto/deepa/h3k9me3/bam/K_input_rmdup.bam \
+-o /home/roberto/deepa/h3k9me3/bw/K_input.bw
+
+####
+# K_k9me3
+samtools merge -h /home/roberto/deepa/h3k9me3/bam/K_k9me3_L001_Aligned.sortedByCoord.out.bam /home/roberto/deepa/h3k9me3/bam/K_k9me3.bam
+/home/roberto/deepa/h3k9me3/bam/K_k9me3_L001_Aligned.sortedByCoord.out.bam \
+/home/roberto/deepa/h3k9me3/bam/K_k9me3_L002_Aligned.sortedByCoord.out.bam \
+/home/roberto/deepa/h3k9me3/bam/K_k9me3_L003_Aligned.sortedByCoord.out.bam \
+/home/roberto/deepa/h3k9me3/bam/K_k9me3_L004_Aligned.sortedByCoord.out.bam 
+
+java -jar /home/roberto/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
+I=/home/roberto/deepa/h3k9me3/bam/K_k9me3.bam \
+O=/home/roberto/deepa/h3k9me3/bam/K_k9me3_rmdup.bam \
+M=/home/roberto/deepa/h3k9me3/bam/K_k9me3.mfile
+
+samtools index /home/roberto/deepa/h3k9me3/bam/K_k9me3_rmdup.bam
+
+bamCoverage -p max -bs 1 --normalizeUsingRPKM -b /home/roberto/deepa/h3k9me3/bam/K_k9me3_rmdup.bam \
+-o /home/roberto/deepa/h3k9me3/bw/K_k9me3.bw
+
+#
