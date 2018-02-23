50552ffd79551116d60c00af7c14fd90a0754a7c
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-02-24 00:03:12 +0800

    Create deepa_h3k9me3.sh

---
+++ deepa_h3k9me3.sh
@@ -0,0 +1,3 @@
+
+/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --runThreadN 35 --runMode genomeGenerate --genomeDir /home/roberto/references/ \
+--genomeFastaFiles /home/roberto/references/hg38.fa
