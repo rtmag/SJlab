3711cb6222759176a29104c94d10ec7cbc1b4d2d
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-03-12 15:34:27 +0800

    Update deepa_h3k9me3_piechart.R

--- deepa_h3k9me3_piechart.R
+++ deepa_h3k9me3_piechart.R
@@ -3,10 +3,10 @@
 #annotatePeaks.pl H3_up.bed hg38 -annStats H3_up.annStats > H3_up.anno
 #annotatePeaks.pl k9me3_down.bed hg38 -annStats k9me3_down.annStats > k9me3_down.anno
 #annotatePeaks.pl k9me3_up.bed hg38 -annStats k9me3_up.annStats > k9me3_up.anno
-#pdf("H3_down_annStats.pdf")
-postscript("H3_down_annStats.ps")
+pdf("H3_down_annStats.pdf")
+#postscript("H3_down_annStats.ps")
 par(mar=c(5.1,5.1,4.1,5))
-res=read.table(pipe("more H3_down.annStats |grep -v '0.0'|cut -f1,2,4"), sep="\t",header=F)
+res=read.table(pipe("more H3_down.annStats|cut -f1,2,4"), sep="\t",header=F)
 i1 = which(res[,1]=="Annotation")[2]+1
 i2 = dim(res)[1]
 res = res[ i1:i2,]
@@ -17,9 +17,9 @@ tdown = tdown[tdown>16]
 pie(tdown, main="Distribution of H3 peaks observed in\nsiControl compared to siTIP60 (12,586 peaks)")
 dev.off()
 
-#pdf("H3_up_annStats.pdf")
-postscript("H3_up_annStats.ps")
-res=read.table(pipe("more H3_up.annStats |grep -v '0.0'|cut -f1,2,4"), sep="\t",header=F)
+pdf("H3_up_annStats.pdf")
+#postscript("H3_up_annStats.ps")
+res=read.table(pipe("more H3_up.annStats |cut -f1,2,4"), sep="\t",header=F)
 i1 = which(res[,1]=="Annotation")[2]+1
 i2 = dim(res)[1]
 res = res[ i1:i2,]
@@ -31,9 +31,9 @@ pie(tdown, main="Distribution of H3 peaks observed in\nsiTIP60 compared to siCon
 dev.off()
 
 ###
-#pdf("H3K9me3_down_annStats.pdf")
-postscript("H3K9me3_down_annStats.ps")
-res=read.table(pipe("more k9me3_down.annStats |grep -v '0.0'|cut -f1,2,4"), sep="\t",header=F)
+pdf("H3K9me3_down_annStats.pdf")
+#postscript("H3K9me3_down_annStats.ps")
+res=read.table(pipe("more k9me3_down.annStats |cut -f1,2,4"), sep="\t",header=F)
 i1 = which(res[,1]=="Annotation")[2]+1
 i2 = dim(res)[1]
 res = res[ i1:i2,]
@@ -44,9 +44,9 @@ tdown = tdown[tdown>16]
 pie(tdown, main="Distribution of H3k9me3 peaks observed in\nsiControl compared to siTIP60 (8,389 peaks)")
 dev.off()
 
-#pdf("H3K9me3_up_annStats.pdf")
-postscript("H3K9me3_up_annStats.ps")
-res=read.table(pipe("more k9me3_up.annStats |grep -v '0.0'|cut -f1,2,4"), sep="\t",header=F)
+pdf("H3K9me3_up_annStats.pdf")
+#postscript("H3K9me3_up_annStats.ps")
+res=read.table(pipe("more k9me3_up.annStats |cut -f1,2,4"), sep="\t",header=F)
 i1 = which(res[,1]=="Annotation")[2]+1
 i2 = dim(res)[1]
 res = res[ i1:i2,]
