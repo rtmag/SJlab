8b2b0217d4c6575afc36ef6b79b30c8f4c281d1a
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-03-12 14:38:07 +0800

    Update deepa_h3k9me3_piechart.R

--- deepa_h3k9me3_piechart.R
+++ deepa_h3k9me3_piechart.R
@@ -3,3 +3,7 @@ annotatePeaks.pl H3_down.bed hg38 -annStats H3_down.annStats > H3_down.anno
 annotatePeaks.pl H3_up.bed hg38 -annStats H3_up.annStats > H3_up.anno
 annotatePeaks.pl k9me3_down.bed hg38 -annStats k9me3_down.annStats > k9me3_down.anno
 annotatePeaks.pl k9me3_up.bed hg38 -annStats k9me3_up.annStats > k9me3_up.anno
+
+#par(mar=c(5.1,4.1,4.1,7))
+res=read.table(pipe("more Down_NT.annStats |grep -v '0.0'|cut -f1,2,4|tail -n +10"), sep="\t",header=F)
+
