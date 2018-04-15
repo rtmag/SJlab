c6d14aba65a95a64da36ac3147135ce2793f8cce
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-04-16 02:15:28 +0800

    Create deepa_repenrich2.bash

---
+++ deepa_repenrich2.bash
@@ -0,0 +1,4 @@
+python /root/myPrograms/RepEnrich2/RepEnrich2_setup.py \
+/root/sjlab/deepa/repenrich2_ref/hg38_repeatmasker.txt \
+/root/resources/hg38_allchr.fasta \
+/root/sjlab/deepa/repenrich2_ref/repenrich2_setup_hg38
