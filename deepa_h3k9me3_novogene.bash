9a6513f1f91c04b069b04609d9cde25f29e7cd0a
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-04-16 01:43:23 +0800

    Create deepa_h3k9me3_novogene.bash

---
+++ deepa_h3k9me3_novogene.bash
@@ -0,0 +1,26 @@
+
+trim_galore --illumina --paired -o /root/sjlab/deepa/fastq_novogene \
+c_1_1.fq.gz \
+c_1_2.fq.gz
+
+trim_galore --illumina --paired -o /root/sjlab/deepa/fastq_novogene \
+c_2_1.fq.gz \
+c_2_2.fq.gz
+
+trim_galore --illumina --paired -o /root/sjlab/deepa/fastq_novogene \
+c_3_1.fq.gz \
+c_3_2.fq.gz
+
+trim_galore --illumina --paired -o /root/sjlab/deepa/fastq_novogene \
+c_4_1.fq.gz \
+c_4_2.fq.gz
+
+trim_galore --illumina --paired -o /root/sjlab/deepa/fastq_novogene \
+c_5_1.fq.gz \
+c_5_2.fq.gz
+
+trim_galore --illumina --paired -o /root/sjlab/deepa/fastq_novogene \
+c_6_1.fq.gz \
+c_6_2.fq.gz
+
+######################################################################
