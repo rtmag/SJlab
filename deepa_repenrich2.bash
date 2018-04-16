bdece35896ee2832069adaf416ed85e6028af1e7
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-04-16 20:36:08 +0800

    Update deepa_repenrich2.bash

--- deepa_repenrich2.bash
+++ deepa_repenrich2.bash
@@ -2,3 +2,54 @@ python /root/myPrograms/RepEnrich2/RepEnrich2_setup.py \
 /root/sjlab/deepa/repenrich2_ref/hg38_repeatmasker.txt \
 /root/resources/hg38_allchr.fasta \
 /root/sjlab/deepa/repenrich2_ref/repenrich2_setup_hg38
+
+##
+
+bowtie2 -q -p 16 -x /root/resources/hg38_bowtie2/hg38 \
+-1 <(zcat /root/sjlab/deepa/fastq_novogene/c_1_1_val_1.fq.gz) \
+-2 <(zcat /root/sjlab/deepa/fastq_novogene/c_1_2_val_2.fq.gz) \
+-S /root/sjlab/deepa/bam_novogene/c1.sam
+
+samtools view -bS /root/sjlab/deepa/bam_novogene/c1.sam > /root/sjlab/deepa/bam_novogene/c1.bam
+rm /root/sjlab/deepa/bam_novogene/c1.sam
+
+bowtie2 -q -p 16 -x /root/resources/hg38_bowtie2/hg38 \
+-1 <(zcat /root/sjlab/deepa/fastq_novogene/c_2_1_val_1.fq.gz) \
+-2 <(zcat /root/sjlab/deepa/fastq_novogene/c_2_2_val_2.fq.gz) \
+-S /root/sjlab/deepa/bam_novogene/c2.sam
+
+samtools view -bS /root/sjlab/deepa/bam_novogene/c2.sam > /root/sjlab/deepa/bam_novogene/c2.bam
+rm /root/sjlab/deepa/bam_novogene/c2.sam
+
+bowtie2 -q -p 16 -x /root/resources/hg38_bowtie2/hg38 \
+-1 <(zcat /root/sjlab/deepa/fastq_novogene/c_3_1_val_1.fq.gz) \
+-2 <(zcat /root/sjlab/deepa/fastq_novogene/c_3_2_val_2.fq.gz) \
+-S /root/sjlab/deepa/bam_novogene/c3.sam
+
+samtools view -bS /root/sjlab/deepa/bam_novogene/c3.sam > /root/sjlab/deepa/bam_novogene/c3.bam
+rm /root/sjlab/deepa/bam_novogene/c3.sam
+
+bowtie2 -q -p 16 -x /root/resources/hg38_bowtie2/hg38 \
+-1 <(zcat /root/sjlab/deepa/fastq_novogene/c_4_1_val_1.fq.gz) \
+-2 <(zcat /root/sjlab/deepa/fastq_novogene/c_4_2_val_2.fq.gz) \
+-S /root/sjlab/deepa/bam_novogene/c4.sam
+
+samtools view -bS /root/sjlab/deepa/bam_novogene/c4.sam > /root/sjlab/deepa/bam_novogene/c4.bam
+rm /root/sjlab/deepa/bam_novogene/c4.sam
+
+bowtie2 -q -p 16 -x /root/resources/hg38_bowtie2/hg38 \
+-1 <(zcat /root/sjlab/deepa/fastq_novogene/c_5_1_val_1.fq.gz) \
+-2 <(zcat /root/sjlab/deepa/fastq_novogene/c_5_2_val_2.fq.gz) \
+-S /root/sjlab/deepa/bam_novogene/c5.sam
+
+samtools view -bS /root/sjlab/deepa/bam_novogene/c5.sam > /root/sjlab/deepa/bam_novogene/c5.bam
+rm /root/sjlab/deepa/bam_novogene/c5.sam
+
+bowtie2 -q -p 16 -x /root/resources/hg38_bowtie2/hg38 \
+-1 <(zcat /root/sjlab/deepa/fastq_novogene/c_6_1_val_1.fq.gz) \
+-2 <(zcat /root/sjlab/deepa/fastq_novogene/c_6_2_val_2.fq.gz) \
+-S /root/sjlab/deepa/bam_novogene/c6.sam
+
+samtools view -bS /root/sjlab/deepa/bam_novogene/c6.sam > /root/sjlab/deepa/bam_novogene/c6.bam
+rm /root/sjlab/deepa/bam_novogene/c6.sam
+
