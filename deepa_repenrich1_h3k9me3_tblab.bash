9169190675dd02db158c98adad7982ef032ab380
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-04-25 13:56:27 +0800

    Update deepa_repenrich1_h3k9me3_tblab.bash

--- deepa_repenrich1_h3k9me3_tblab.bash
+++ deepa_repenrich1_h3k9me3_tblab.bash
@@ -7,7 +7,7 @@ bowtie-build /root/resources/hg38_allchr.fasta /root/resources/hg38_bowtie/hg38
 --max /root/sjlab/deepa_repenrich1/bowtie/siControl_H3_steph_multimap.fastq \
 -1 <(zcat /root/sjlab/deepa/fastq_steph/C-H3_R1.fq.gz) \
 -2 <(zcat /root/sjlab/deepa/fastq_steph/C-H3_R2.fq.gz) \
-/root/sjlab/deepa_repenrich1/bowtie/siControl_H3_steph.sam > /root/sjlab/deepa_repenrich1/bowtie/siControl_H3_steph.log
+/root/sjlab/deepa_repenrich1/bowtie/siControl_H3_steph.sam 2> /root/sjlab/deepa_repenrich1/bowtie/siControl_H3_steph.log
 
 samtools view -bS /root/sjlab/deepa_repenrich1/bowtie/siControl_H3_steph.sam > /root/sjlab/deepa_repenrich1/bowtie/siControl_H3_steph.bam
 rm /root/sjlab/deepa_repenrich1/bowtie/siControl_H3_steph.sam
@@ -19,7 +19,7 @@ samtools index /root/sjlab/deepa_repenrich1/bowtie/siControl_H3_steph_sort.bam
 --max /root/sjlab/deepa_repenrich1/bowtie/siControl_H3K9me3_steph_multimap.fastq \
 -1 <(zcat /root/sjlab/deepa/fastq_steph/C-k9me3_R1.fq.gz) \
 -2 <(zcat /root/sjlab/deepa/fastq_steph/C-k9me3_R2.fq.gz) \
-/root/sjlab/deepa_repenrich1/bowtie/siControl_H3K9me3_steph.sam > /root/sjlab/deepa_repenrich1/bowtie/siControl_H3K9me3_steph.log
+/root/sjlab/deepa_repenrich1/bowtie/siControl_H3K9me3_steph.sam 2> /root/sjlab/deepa_repenrich1/bowtie/siControl_H3K9me3_steph.log
 
 samtools view -bS /root/sjlab/deepa_repenrich1/bowtie/siControl_H3K9me3_steph.sam > /root/sjlab/deepa_repenrich1/bowtie/siControl_H3K9me3_steph.bam
 rm /root/sjlab/deepa_repenrich1/bowtie/siControl_H3_steph.sam
@@ -31,7 +31,7 @@ samtools index root/sjlab/deepa_repenrich1/bowtie/siControl_H3_steph_sort.bam
 --max /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3_steph_multimap.fastq \
 -1 <(zcat /root/sjlab/deepa/fastq_steph/K-H3_R1.fq.gz) \
 -2 <(zcat /root/sjlab/deepa/fastq_steph/K-H3_R2.fq.gz) \
-/root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3_steph.sam > /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3_steph.log
+/root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3_steph.sam 2> /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3_steph.log
 
 samtools view -bS /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3_steph.sam > /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3_steph.bam
 rm /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3_steph.sam
@@ -43,7 +43,7 @@ samtools index /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3_steph_sort.bam
 --max /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3K9me3_steph_multimap.fastq \
 -1 <(zcat /root/sjlab/deepa/fastq_steph/K-k9me3_R1.fq.gz) \
 -2 <(zcat /root/sjlab/deepa/fastq_steph/K-k9me3_R2.fq.gz) \
-/root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3K9me3_steph.sam > /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3K9me3_steph.log
+/root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3K9me3_steph.sam 2> /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3K9me3_steph.log
 
 samtools view -bS /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3K9me3_steph.sam > /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3K9me3_steph.bam
 rm /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3K9me3_steph.sam
@@ -57,7 +57,7 @@ samtools index /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3K9me3_steph_sort.ba
 --max /root/sjlab/deepa_repenrich1/bowtie/siControl_H3_novogene_multimap.fastq \
 -1 <(zcat /root/sjlab/deepa/fastq_novogene/c_3_1_val_1.fq.gz) \
 -2 <(zcat /root/sjlab/deepa/fastq_novogene/c_3_2_val_2.fq.gz) \
-/root/sjlab/deepa_repenrich1/bowtie/siControl_H3_novogene.sam > /root/sjlab/deepa_repenrich1/bowtie/siControl_H3_novogene.log
+/root/sjlab/deepa_repenrich1/bowtie/siControl_H3_novogene.sam 2> /root/sjlab/deepa_repenrich1/bowtie/siControl_H3_novogene.log
 
 samtools view -bS /root/sjlab/deepa_repenrich1/bowtie/siControl_H3_novogene.sam > /root/sjlab/deepa_repenrich1/bowtie/siControl_H3_novogene.bam
 rm /root/sjlab/deepa_repenrich1/bowtie/siControl_H3_novogene.sam
@@ -69,7 +69,7 @@ samtools index /root/sjlab/deepa_repenrich1/bowtie/siControl_H3_novogene_sort.ba
 --max /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3_novogene_multimap.fastq \
 -1 <(zcat /root/sjlab/deepa/fastq_novogene/c_4_1_val_1.fq.gz) \
 -2 <(zcat /root/sjlab/deepa/fastq_novogene/c_4_2_val_2.fq.gz) \
-/root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3_novogene.sam > /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3_novogene.log
+/root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3_novogene.sam 2> /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3_novogene.log
 
 samtools view -bS /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3_novogene.sam > /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3_novogene.bam
 rm /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3_novogene.sam
@@ -81,7 +81,7 @@ samtools index /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3_novogene_sort.bam
 --max /root/sjlab/deepa_repenrich1/bowtie/siControl_H3K9me3_novogene_multimap.fastq \
 -1 <(zcat /root/sjlab/deepa/fastq_novogene/c_5_1_val_1.fq.gz) \
 -2 <(zcat /root/sjlab/deepa/fastq_novogene/c_5_2_val_2.fq.gz) \
-/root/sjlab/deepa_repenrich1/bowtie/siControl_H3K9me3_novogene.sam > /root/sjlab/deepa_repenrich1/bowtie/siControl_H3K9me3_novogene.log
+/root/sjlab/deepa_repenrich1/bowtie/siControl_H3K9me3_novogene.sam 2> /root/sjlab/deepa_repenrich1/bowtie/siControl_H3K9me3_novogene.log
 
 samtools view -bS /root/sjlab/deepa_repenrich1/bowtie/siControl_H3K9me3_novogene.sam > /root/sjlab/deepa_repenrich1/bowtie/siControl_H3K9me3_novogene.bam
 rm /root/sjlab/deepa_repenrich1/bowtie/siControl_H3K9me3_novogene.sam
@@ -93,7 +93,7 @@ samtools index /root/sjlab/deepa_repenrich1/bowtie/siControl_H3K9me3_novogene_so
 --max /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3K9me3_novogene_multimap.fastq \
 -1 <(zcat /root/sjlab/deepa/fastq_novogene/c_6_1_val_1.fq.gz) \
 -2 <(zcat /root/sjlab/deepa/fastq_novogene/c_6_2_val_2.fq.gz) \
-/root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3K9me3_novogene.sam > /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3K9me3_novogene.log
+/root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3K9me3_novogene.sam 2> /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3K9me3_novogene.log
 
 samtools view -bS /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3K9me3_novogene.sam > /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3K9me3_novogene.bam
 rm /root/sjlab/deepa_repenrich1/bowtie/siTIP60_H3K9me3_novogene.sam
