4e8c66ea6360c5cbc17b5d52196c123ebe4d826d
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-05-22 17:20:10 +0800

    Update deepa_h3k9me3_rnaseq_integration.R

--- deepa_h3k9me3_rnaseq_integration.R
+++ deepa_h3k9me3_rnaseq_integration.R
@@ -244,3 +244,41 @@ legend("bottomleft",legend=sum(rna$log2FoldChange<(-1) & k9me3<(-1)),bty = "n",b
 binded=cbind(rna$log2FoldChange,k9me3)
 colnames(binded) = c("RNA_seq_log2FC","K9me3_normH3_log2FC")
 dev.off()
+#######
+
+
+vsd = readRDS("H3_H3K9me3_tss1kb_vsd.rds")
+
+k9me3 = k9me3[order(names(k9me3))]
+
+rna = readRDS("deseq2_siC_vs_siK.rds")
+rna = rna[rownames(rna) %in% names(k9me3),]
+rna = rna[!duplicated(rownames(rna)),]
+rna = rna[order(rownames(rna)),]
+
+ix = which(abs(rna$log2FoldChange)>1 & rna$padj<0.05)
+
+rna = rna[ ix ,]
+
+k9me3 = (vsd[,3]-vsd[,1])-(vsd[,4]-vsd[,2])
+k9me3 = k9me3[order(names(k9me3))]
+
+k9me3 = k9me3[ ix ]
+
+table(names(k9me3)==rownames(rna))
+
+
+pdf("rna_k9me3_tss1kb_scatterplot_DEgenes.pdf")
+
+smoothScatter(rna$log2FoldChange,k9me3,
+             xlab=expression('RNA-Seq Log'[2]*' Fold Change ( siControl / siTIP60 )'),
+             ylab=expression('H3K9me3/H3 Log'[2]*' Fold Change TSS-1kb ( siControl / siTIP60 )'))
+abline(v=0)
+abline(h=0)
+
+
+legend("topright",legend=sum(rna$log2FoldChange>0 & k9me3>0),bty = "n",border=NA)
+legend("topleft",legend=sum(rna$log2FoldChange<(0) & k9me3>0),bty = "n",border=NA)
+legend("bottomright",legend=sum(rna$log2FoldChange>0 & k9me3<(0)),bty = "n",border=NA)
+legend("bottomleft",legend=sum(rna$log2FoldChange<(0) & k9me3<(0)),bty = "n",border=NA)
+dev.off()
