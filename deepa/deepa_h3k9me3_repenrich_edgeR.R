c081292c778fa081cb16db8a092e53f21dbecaa4
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-05-31 21:11:19 +0800

    Update deepa_h3k9me3_repenrich_edgeR.R

--- deepa/deepa_h3k9me3_repenrich_edgeR.R
+++ deepa/deepa_h3k9me3_repenrich_edgeR.R
@@ -32,8 +32,8 @@ counts <- data.frame(
 # # of reads processed - # reads that failed to align
 meta <- data.frame(
 	row.names=colnames(counts),
-	condition=c("siC","siC","siK","siK","DMSO","DMSO","JQ1","JQ1"),
-	libsize=c(4254957,6045550,6563529,6769278,5100955,4492173,6613606,6288050)
+	condition=c("siC_h3k9me3","siC_h3k9me3","siK_h3k9me3","siK_h3k9me3","siC_h3","siC_h3","siK_h3","siK_h3"),
+	libsize=  c(73350175,       57558823,      76696799,     54267891,  74401979,59562768,76002273,57180974)
 )
 
 # Define the library size and conditions for the GLM
