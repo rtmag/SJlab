f03cf0b8756686da596e049f21b034055ced8cea
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-05-31 21:32:23 +0800

    Update deepa_h3k9me3_repenrich_edgeR.R

--- deepa/deepa_h3k9me3_repenrich_edgeR.R
+++ deepa/deepa_h3k9me3_repenrich_edgeR.R
@@ -43,3 +43,105 @@ design <- model.matrix(~0+condition)
 colnames(design) <- levels(meta$condition)
 
 
+# Build a DGE object for the GLM
+y <- DGEList(counts=counts, lib.size=libsize)
+
+# Normalize the data
+y <- calcNormFactors(y)
+y$samples
+plotMDS(y)
+
+# Estimate the variance
+y <- estimateGLMCommonDisp(y, design)
+y <- estimateGLMTrendedDisp(y, design)
+y <- estimateGLMTagwiseDisp(y, design)
+plotBCV(y)
+
+# Build an object to contain the normalized read abundance
+logcpm <- cpm(y, log=TRUE, lib.size=libsize)
+logcpm <- as.data.frame(logcpm)
+colnames(logcpm) <- factor(meta$condition)
+
+# Conduct fitting of the GLM
+yfit <- glmFit(y, design)
+
+# Initialize result matrices to contain the results of the GLM
+results <- matrix(nrow=dim(counts)[1],ncol=0)
+logfc <- matrix(nrow=dim(counts)[1],ncol=0)
+
+
+# Make the comparisons for the GLM
+my.contrasts <- makeContrasts( siC_siK_h3k9me3 =  siC_h3k9me3 - siK_h3k9me3,
+			      siC_siK_h3 = siC_h3 - siK_h3,
+			      levels = design )  
+
+# Define the contrasts used in the comparisons
+allcontrasts = c(
+	"siC_siK_h3k9me3",
+	"siC_siK_h3"
+)
+
+
+# Conduct a for loop that will do the fitting of the GLM for each comparison
+# Put the results into the results objects
+for(current_contrast in allcontrasts) {
+	lrt <- glmLRT(yfit, contrast=my.contrasts[,current_contrast])
+	plotSmear(lrt, de.tags=rownames(y))
+	title(current_contrast)
+	res <- topTags(lrt,n=dim(c)[1],sort.by="none")$table
+	colnames(res) <- paste(colnames(res),current_contrast,sep=".")
+	results <- cbind(results,res[,c(1,5)])
+	logfc <- cbind(logfc,res[c(1)])
+}
+
+# Add the repeat types back into the results.
+# We should still have the same order as the input data
+results$class <- siC_r1[,2]
+results$type <- siC_r1[,3]
+
+# Sort the results table by the logFC
+results <- results[with(results, order(-abs(logFC.siC_siK))), ]
+results <- cbind(results,rownames(results))
+colnames(results)[7]="repeats"
+# Save the results
+write.table(results, 'results.txt', quote=FALSE, sep="\t")
+results_ori=results
+
+
+#FDR 5%
+results=results_ori
+
+for(current_contrast in allcontrasts) {
+  logFC <- results[ results[, paste0("FDR.", current_contrast)]<0.05, 
+		   paste0("logFC.", current_contrast)]
+  # Plot the repeat classes
+	
+postscript(paste(current_contrast,"_class_replicates_repenrich_FDR_5_noFilter.ps",sep=""))
+  classes <- with(results[results[, paste0("FDR.", current_contrast)]<0.05,], reorder(class, -logFC, median))
+  par(mar=c(6,10,4,1))
+  boxplot(logFC ~ as.vector(classes), data=results, outline=FALSE, horizontal=TRUE,
+          las=2, xlab="log2(Fold Change)", main=paste("Class",current_contrast,"FDR 5%") )
+  abline(v=0)
+dev.off()
+	
+postscript(paste(current_contrast,"_type_replicates_repenrich_FDR_5_noFilter.ps",sep=""))
+  # Plot the repeat types
+	
+	  par(mar=c(6,10,4,1))
+  types <- with(results[results[, paste0("FDR.", current_contrast)]<0.05,], reorder(type, -logFC, median))
+    boxplot(logFC ~ as.vector(types), data=results, outline=FALSE, horizontal=TRUE,
+          las=2, xlab="log2(Fold Change)", main=paste("Type",current_contrast,"FDR 5%") )
+  abline(v=0)
+dev.off()
+
+postscript(paste(current_contrast,"_repeats_replicates_repenrich_FDR_5_noFilter.ps",sep=""))
+	#plot repeats
+		  par(mar=c(6,10,4,1),cex.axis=.4)
+  repe <- with(results[results[, paste0("FDR.", current_contrast)]<0.05,], reorder(repeats, -logFC, median))
+boxplot(logFC ~ as.vector(repe), data=results, outline=FALSE, horizontal=TRUE,
+          las=2, xlab="log2(Fold Change)", main=paste("Repeat",current_contrast,"FDR 5%"))
+  abline(v=0)
+		  par(mar=c(6,10,4,1),cex.axis=1)
+	dev.off()
+
+}
