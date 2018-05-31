4f9b06c56b268610312467ac52c552a441819f34
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-05-31 22:46:01 +0800

    Update deepa_h3k9me3_repenrich_deseq2.R

--- deepa/deepa_h3k9me3_repenrich_deseq2.R
+++ deepa/deepa_h3k9me3_repenrich_deseq2.R
@@ -7,15 +7,15 @@ options(scipen=999)
 # counts <- read.csv(file = "counts.csv")
 
 # In the case of seperate outputs, load the RepEnrich results - fraction counts
-siC_h3k9me3_novogene <- read.delim('/home/rtm/CSI/sjlab/repenrich/siControl_H3K9me3_novogene_fraction_counts.txt', header=FALSE)
-siC_h3k9me3_steph <- read.delim('/home/rtm/CSI/sjlab/repenrich/siControl_H3K9me3_steph_fraction_counts.txt', header=FALSE)
-siC_h3_novogene <- read.delim('/home/rtm/CSI/sjlab/repenrich/siControl_H3_novogene_fraction_counts.txt', header=FALSE)
-siC_h3_steph <- read.delim('/home/rtm/CSI/sjlab/repenrich/siControl_H3_steph_fraction_counts.txt', header=FALSE)
+siC_h3k9me3_novogene <- read.delim('repenrich/siControl_H3K9me3_novogene_fraction_counts.txt', header=FALSE)
+siC_h3k9me3_steph <- read.delim('repenrich/siControl_H3K9me3_steph_fraction_counts.txt', header=FALSE)
+siC_h3_novogene <- read.delim('repenrich/siControl_H3_novogene_fraction_counts.txt', header=FALSE)
+siC_h3_steph <- read.delim('repenrich/siControl_H3_steph_fraction_counts.txt', header=FALSE)
 
-siK_h3k9me3_novogene <- read.delim('/home/rtm/CSI/sjlab/repenrich/siTIP60_H3K9me3_novogene_fraction_counts.txt', header=FALSE)
-siK_h3k9me3_steph <- read.delim('/home/rtm/CSI/sjlab/repenrich/siTIP60_H3K9me3_steph_fraction_counts.txt', header=FALSE)
-siK_h3_novogene <- read.delim('/home/rtm/CSI/sjlab/repenrich/siTIP60_H3_novogene_fraction_counts.txt', header=FALSE)
-siK_h3_steph <- read.delim('/home/rtm/CSI/sjlab/repenrich/siTIP60_H3_steph_fraction_counts.txt', header=FALSE)
+siK_h3k9me3_novogene <- read.delim('repenrich/siTIP60_H3K9me3_novogene_fraction_counts.txt', header=FALSE)
+siK_h3k9me3_steph <- read.delim('repenrich/siTIP60_H3K9me3_steph_fraction_counts.txt', header=FALSE)
+siK_h3_novogene <- read.delim('repenrich/siTIP60_H3_novogene_fraction_counts.txt', header=FALSE)
+siK_h3_steph <- read.delim('repenrich/siTIP60_H3_steph_fraction_counts.txt', header=FALSE)
 
 #' Build a counts table
 countData <- data.frame(
