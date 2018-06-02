4a0eebe86ac29e9035734695e4556141258b5cc9
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-06-02 16:55:59 +0800

    Create QC_chip-seq.bash

---
+++ deepa/QC_chip-seq.bash
@@ -0,0 +1,28 @@
+#ls -1 *sort.bam|perl -pe 's/^/samtools index /g'|perl -pe 's/\n/ &\n/g'
+samtools index siControl_H3K9me3_novogene_sort.bam &
+samtools index siControl_H3K9me3_steph_sort.bam &
+samtools index siControl_H3_novogene_sort.bam &
+samtools index siControl_H3_steph_sort.bam &
+samtools index siTIP60_H3K9me3_novogene_sort.bam &
+samtools index siTIP60_H3K9me3_steph_sort.bam &
+samtools index siTIP60_H3_novogene_sort.bam &
+samtools index siTIP60_H3_steph_sort.bam &
+
+#ls -1 *sort.bam|perl -pe 's/(.+)\_sort\.bam/ bamCoverage \-p max \-bs 1 \-\-normalizeUsing CPM \-b $1\_sort\.bam \-o $1\.bw /g'
+
+ bamCoverage -p max -bs 1 --normalizeUsing CPM -b siControl_H3K9me3_novogene_sort.bam -o /root/sjlab/deepa_repenrich1/bw/siControl_H3K9me3_novogene.bw 
+ bamCoverage -p max -bs 1 --normalizeUsing CPM -b siControl_H3K9me3_steph_sort.bam -o /root/sjlab/deepa_repenrich1/bw/siControl_H3K9me3_steph.bw 
+ bamCoverage -p max -bs 1 --normalizeUsing CPM -b siControl_H3_novogene_sort.bam -o /root/sjlab/deepa_repenrich1/bw/siControl_H3_novogene.bw 
+ bamCoverage -p max -bs 1 --normalizeUsing CPM -b siControl_H3_steph_sort.bam -o /root/sjlab/deepa_repenrich1/bw/siControl_H3_steph.bw 
+ bamCoverage -p max -bs 1 --normalizeUsing CPM -b siTIP60_H3K9me3_novogene_sort.bam -o /root/sjlab/deepa_repenrich1/bw/siTIP60_H3K9me3_novogene.bw 
+ bamCoverage -p max -bs 1 --normalizeUsing CPM -b siTIP60_H3K9me3_steph_sort.bam -o /root/sjlab/deepa_repenrich1/bw/siTIP60_H3K9me3_steph.bw 
+ bamCoverage -p max -bs 1 --normalizeUsing CPM -b siTIP60_H3_novogene_sort.bam -o /root/sjlab/deepa_repenrich1/bw/siTIP60_H3_novogene.bw 
+ bamCoverage -p max -bs 1 --normalizeUsing CPM -b siTIP60_H3_steph_sort.bam -o /root/sjlab/deepa_repenrich1/bw/siTIP60_H3_steph.bw 
+ 
+ ###################################
+ 
+ multiBigwigSummary -p max -bs 200 -b siControl_H3K9me3_novogene.bw siControl_H3K9me3_steph.bw siControl_H3_novogene.bw siControl_H3_steph.bw \
+siTIP60_H3K9me3_novogene.bw siTIP60_H3K9me3_steph.bw siTIP60_H3_novogene.bw siTIP60_H3_steph.bw  -o multiBigwig.npz
+
+plotPCA --corData multiBigwig.npz -o multiBigwig_pca.pdf
+plotCorrelation --corData multiBigwig.npz -c spearman -o multiBigwig_plotcorrelation.pdf
