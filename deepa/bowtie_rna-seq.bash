a1dd071629e814b0a6b49508e189ebb18771a13f
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-06-06 01:33:33 +0800

    Update bowtie_rna-seq.bash

--- deepa/bowtie_rna-seq.bash
+++ deepa/bowtie_rna-seq.bash
@@ -4,56 +4,56 @@ bowtie /home/roberto/references/hg38_bowtie/hg38 -p 45 -t -m 1 -S --chunkmbs 400
 --max /home/roberto/deepa/novogene/repenrich_bowtie2/HCT_siControl_HWN2YCCXX_L5_multimap.fastq \
 -1 <( zcat /home/roberto/deepa/novogene/trimmed/HCT_siControl_HWN2YCCXX_L5_1_val_1.fq.gz ) \
 -2 <( zcat /home/roberto/deepa/novogene/trimmed/HCT_siControl_HWN2YCCXX_L5_2_val_2.fq.gz ) \
-/home/roberto/deepa/novogene/repenrich_bowtie2/HCT_siControl_HWN2YCCXX_L5_uniq.sam
+/home/roberto/deepa/novogene/repenrich_bowtie2/HCT_siControl_HWN2YCCXX_L5_uniq.sam 2> /home/roberto/deepa/novogene/repenrich_bowtie2/HCT_siControl.log
 
 #2 old siK
 bowtie /home/roberto/references/hg38_bowtie/hg38 -p 45 -t -m 1 -S --chunkmbs 4000 -X 1000 \
 --max /home/roberto/deepa/novogene/repenrich_bowtie2/HCT_siK_HVNYLCCXX_L2_multimap.fastq \
 -1 <( zcat /home/roberto/deepa/novogene/trimmed/HCT_siK_HVNYLCCXX_L2_1_val_1.fq.gz ) \
 -2 <( zcat /home/roberto/deepa/novogene/trimmed/HCT_siK_HVNYLCCXX_L2_2_val_2.fq.gz ) \
-/home/roberto/deepa/novogene/repenrich_bowtie2/HCT_siK_HVNYLCCXX_L2_uniq.sam
+/home/roberto/deepa/novogene/repenrich_bowtie2/HCT_siK_HVNYLCCXX_L2_uniq.sam 2> /home/roberto/deepa/novogene/repenrich_bowtie2/HCT_siK.log
 
 #3 new siC
 bowtie /home/roberto/references/hg38_bowtie/hg38 -p 45 -t -m 1 -S --chunkmbs 4000 -X 1000 \
 --max /home/roberto/deepa/novogene/repenrich_bowtie2/HCT116_siC_multimap.fastq \
 -1 <( zcat /home/roberto/deepa/novogene/trimmed/HCT116_siC_1_val_1.fq.gz ) \
 -2 <( zcat /home/roberto/deepa/novogene/trimmed/HCT116_siC_2_val_2.fq.gz ) \
-/home/roberto/deepa/novogene/repenrich_bowtie2/HCT116_siC_uniq.sam
+/home/roberto/deepa/novogene/repenrich_bowtie2/HCT116_siC_uniq.sam 2> /home/roberto/deepa/novogene/repenrich_bowtie2/HCT116_siC.log
 
 #4 new siK
 bowtie /home/roberto/references/hg38_bowtie/hg38 -p 45 -t -m 1 -S --chunkmbs 4000 -X 1000 \
 --max /home/roberto/deepa/novogene/repenrich_bowtie2/HCT116_siK_multimap.fastq \
 -1 <( zcat /home/roberto/deepa/novogene/trimmed/HCT116_siK_1_val_1.fq.gz ) \
 -2 <( zcat /home/roberto/deepa/novogene/trimmed/HCT116_siK_2_val_2.fq.gz ) \
-/home/roberto/deepa/novogene/repenrich_bowtie2/HCT116_siK_uniq.sam
+/home/roberto/deepa/novogene/repenrich_bowtie2/HCT116_siK_uniq.sam 2> /home/roberto/deepa/novogene/repenrich_bowtie2/HCT116_siK.log
 
 #5 old DMSO
 bowtie /home/roberto/references/hg38_bowtie/hg38 -p 45 -t -m 1 -S --chunkmbs 4000 -X 1000 \
 --max /home/roberto/deepa/novogene/repenrich_bowtie2/HCT_DMSO_HWN2YCCXX_L2_multimap.fastq \
 -1 <( zcat /home/roberto/deepa/novogene/trimmed/HCT_DMSO_HWN2YCCXX_L2_1_val_1.fq.gz ) \
 -2 <( zcat /home/roberto/deepa/novogene/trimmed/HCT_DMSO_HWN2YCCXX_L2_2_val_2.fq.gz ) \
-/home/roberto/deepa/novogene/repenrich_bowtie2/HCT_DMSO_HWN2YCCXX_L2_uniq.sam
+/home/roberto/deepa/novogene/repenrich_bowtie2/HCT_DMSO_HWN2YCCXX_L2_uniq.sam 2> /home/roberto/deepa/novogene/repenrich_bowtie2/HCT_DMSO.log
 
 #6 old JQ1
 bowtie /home/roberto/references/hg38_bowtie/hg38 -p 45 -t -m 1 -S --chunkmbs 4000 -X 1000 \
 --max /home/roberto/deepa/novogene/repenrich_bowtie2/HCT_JQ1_HWN2YCCXX_L5_multimap.fastq \
 -1 <( zcat /home/roberto/deepa/novogene/trimmed/HCT_JQ1_HWN2YCCXX_L5_1_val_1.fq.gz ) \
 -2 <( zcat /home/roberto/deepa/novogene/trimmed/HCT_JQ1_HWN2YCCXX_L5_2_val_2.fq.gz ) \
-/home/roberto/deepa/novogene/repenrich_bowtie2/HCT_JQ1_HWN2YCCXX_L5_uniq.sam
+/home/roberto/deepa/novogene/repenrich_bowtie2/HCT_JQ1_HWN2YCCXX_L5_uniq.sam 2> /home/roberto/deepa/novogene/repenrich_bowtie2/HCT_JQ1.log
 
 #7 new DMSO
 bowtie /home/roberto/references/hg38_bowtie/hg38 -p 45 -t -m 1 -S --chunkmbs 4000 -X 1000 \
 --max /home/roberto/deepa/novogene/repenrich_bowtie2/HCT116_siC_DMSO_multimap.fastq \
 -1 <( zcat /home/roberto/deepa/novogene/trimmed/HCT116_siC_DMSO_1_val_1.fq.gz ) \
 -2 <( zcat /home/roberto/deepa/novogene/trimmed/HCT116_siC_DMSO_2_val_2.fq.gz ) \
-/home/roberto/deepa/novogene/repenrich_bowtie2/HCT116_siC_DMSO_uniq.sam
+/home/roberto/deepa/novogene/repenrich_bowtie2/HCT116_siC_DMSO_uniq.sam 2> /home/roberto/deepa/novogene/repenrich_bowtie2/HCT116_siC_DMSO.log
 
 #8 new JQ1
 bowtie /home/roberto/references/hg38_bowtie/hg38 -p 45 -t -m 1 -S --chunkmbs 4000 -X 1000 \
 --max /home/roberto/deepa/novogene/repenrich_bowtie2/HCT116_siC_JQ1_multimap.fastq \
 -1 <( zcat /home/roberto/deepa/novogene/trimmed/HCT116_siC_JQ1_1_val_1.fq.gz ) \
 -2 <( zcat /home/roberto/deepa/novogene/trimmed/HCT116_siC_JQ1_2_val_2.fq.gz ) \
-/home/roberto/deepa/novogene/repenrich_bowtie2/HCT116_siC_JQ1_uniq.sam
+/home/roberto/deepa/novogene/repenrich_bowtie2/HCT116_siC_JQ1_uniq.sam 2> /home/roberto/deepa/novogene/repenrich_bowtie2/HCT116_siC_JQ1.log
 
 #####
 cd /home/roberto/deepa/novogene/repenrich_bowtie2/
