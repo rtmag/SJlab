2c5e00b7fb5cc5fcbbae66d3ac7a19d64cbb1082
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-06-06 00:10:42 +0800

    Update h3_h3k9me3_repenrich_steph.R

--- deepa/h3_h3k9me3_repenrich_steph.R
+++ deepa/h3_h3k9me3_repenrich_steph.R
@@ -130,7 +130,7 @@ par(mar=c(7,1.1,1.1,6))
 x=(family[,1]-family[,2])
 names(x)=rownames(family)
 x=sort(x)
-x = x[names(x) %in% c("tRNA","hAT-Tip100?","hAT-Tip100","hAT-Charlie","hAT-Blackjack","hAT","TcMar-Tigger","Piggybac","Other","L1",
+x = x[names(x) %in% c("hAT-Tip100?","hAT-Tip100","hAT-Charlie","hAT-Blackjack","hAT","TcMar-Tigger","Piggybac","Other","L1",
 		     "Gypsy","ERVL-MaLR","ERVL","ERVK","ERV1","ERV1","Alu") ]
 barplot(t(x),beside=T,border=T,ylim=c(-.3,.3),
 	col=ifelse(x>0,"indianred1","lightblue3"),las=2,cex.names=1,axes = FALSE)
