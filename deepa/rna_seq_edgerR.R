edfd8871283b67461892c53b9bbfee06fbf6173f
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-06-08 13:36:16 +0800

    Create rna_seq_edgerR.R

---
+++ deepa/rna_seq_edgerR.R
@@ -0,0 +1,37 @@
+#FDR 5%
+results=results_ori
+
+for(current_contrast in allcontrasts) {
+  logFC <- results[ results[, paste0("FDR.", current_contrast)]<0.05, 
+		   paste0("logFC.", current_contrast)]
+  # Plot the repeat classes
+	
+postscript(paste(current_contrast,"_class_replicates_repenrich_FDR_5_noFilter.ps",sep=""))
+  classes <- with(results[results[, paste0("FDR.", current_contrast)]<0.05,], reorder(class, -logFC, median))
+  par(mar=c(6,10,4,1))
+  boxplot(logFC ~ as.vector(classes), data=results, outline=FALSE, horizontal=TRUE,
+          las=2, xlab="log2(Fold Change)", main=paste("Class",current_contrast,"FDR 5%") )
+  abline(v=0)
+dev.off()
+	
+postscript(paste(current_contrast,"_type_replicates_repenrich_FDR_5_noFilter.ps",sep=""))
+  # Plot the repeat types
+	
+	  par(mar=c(6,10,4,1))
+  types <- with(results[results[, paste0("FDR.", current_contrast)]<0.05,], reorder(type, -logFC, median))
+    boxplot(logFC ~ as.vector(types), data=results, outline=FALSE, horizontal=TRUE,
+          las=2, xlab="log2(Fold Change)", main=paste("Type",current_contrast,"FDR 5%") )
+  abline(v=0)
+dev.off()
+
+postscript(paste(current_contrast,"_repeats_replicates_repenrich_FDR_5_noFilter.ps",sep=""))
+	#plot repeats
+		  par(mar=c(6,10,4,1),cex.axis=.4)
+  repe <- with(results[results[, paste0("FDR.", current_contrast)]<0.05,], reorder(repeats, -logFC, median))
+boxplot(logFC ~ as.vector(repe), data=results, outline=FALSE, horizontal=TRUE,
+          las=2, xlab="log2(Fold Change)", main=paste("Repeat",current_contrast,"FDR 5%"))
+  abline(v=0)
+		  par(mar=c(6,10,4,1),cex.axis=1)
+	dev.off()
+
+}
