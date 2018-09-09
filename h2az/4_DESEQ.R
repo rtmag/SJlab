ce0e26d0f024b9af1879609d0d118caf0a988c31
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-09-10 03:44:29 +0800

    Update 4_DESEQ.R

--- h2az/4_DESEQ.R
+++ h2az/4_DESEQ.R
@@ -46,3 +46,109 @@ dev.off()
 
 saveRDS(vsd,"h2az_vsd.rds")
 ############################################################################################################
+options(scipen=999)
+library(gplots)
+library(factoextra)
+library(RColorBrewer)
+library(DESeq2)
+
+countData = readRDS("RNASEQ_counts.rds")
+#design<-data.frame(group=c("shrh_siC","tham_siC","zhou_siC",
+#                "shrh_siK","tham_siK","zhou_siK") )
+
+design<- data.frame(treatment=c("siC","siC","siC","siK","siK","siK"),
+                    batch=c("shrh","tham","zhou","shrh","tham","zhou") )
+
+dLRT <- DESeqDataSetFromMatrix(countData = countData, colData = design, design = ~ batch+treatment )
+dLRT <- DESeq(dLRT, test="LRT", full=  ~ batch + treatment, reduced=~batch)
+dLRT_res =  results(dLRT,contrast=c("treatment","siK","siC"))
+
+gmt = read.table("HallMark_e2f_targets.txt",sep = "\t", head=T,stringsAsFactors=F)
+vsd = readRDS("h2az_vsd.rds")
+
+
+pdf("e2f_targets_siginificantChange.pdf")
+sig_vsd = vsd[(rownames(vsd) %in% gmt[,1]) & ((abs(dLRT_res$log2FoldChange)>1 & dLRT_res$padj<0.05)),]
+sig_vsd = sig_vsd[complete.cases(sig_vsd),]
+sig_vsd = sig_vsd[complete.cases(sig_vsd),]
+for(ix in 1:dim(sig_vsd)[1]){
+  sig_vsd[ix,1] = sig_vsd[ix,1]/mean(sig_vsd[ix,c(1,4)])
+  sig_vsd[ix,4] = sig_vsd[ix,4]/mean(sig_vsd[ix,c(1,4)])
+  sig_vsd[ix,2] = sig_vsd[ix,2]/mean(sig_vsd[ix,c(2,5)])
+  sig_vsd[ix,5] = sig_vsd[ix,5]/mean(sig_vsd[ix,c(2,5)])
+  sig_vsd[ix,3] = sig_vsd[ix,3]/mean(sig_vsd[ix,c(3,6)])
+  sig_vsd[ix,6] = sig_vsd[ix,6]/mean(sig_vsd[ix,c(3,6)])
+  }
+
+  colnames(sig_vsd) = c("shrh_siC","tham_siC","zhou_siC",
+                "shrh_siK","tham_siK","zhou_siK")
+  colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(20))
+  heatmap.2(sig_vsd,col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+  xlab="", ylab="E2F Target Genes",key.title="Gene expression",cexCol=.65,cexRow=.6)
+dev.off()
+
+pdf("e2f_targets_ALL.pdf")
+sig_vsd = vsd[(rownames(vsd) %in% gmt[,1]),]
+sig_vsd = sig_vsd[complete.cases(sig_vsd),]
+sig_vsd = sig_vsd[complete.cases(sig_vsd),]
+for(ix in 1:dim(sig_vsd)[1]){
+  sig_vsd[ix,1] = sig_vsd[ix,1]/mean(sig_vsd[ix,c(1,4)])
+  sig_vsd[ix,4] = sig_vsd[ix,4]/mean(sig_vsd[ix,c(1,4)])
+  sig_vsd[ix,2] = sig_vsd[ix,2]/mean(sig_vsd[ix,c(2,5)])
+  sig_vsd[ix,5] = sig_vsd[ix,5]/mean(sig_vsd[ix,c(2,5)])
+  sig_vsd[ix,3] = sig_vsd[ix,3]/mean(sig_vsd[ix,c(3,6)])
+  sig_vsd[ix,6] = sig_vsd[ix,6]/mean(sig_vsd[ix,c(3,6)])
+  }
+
+  colnames(sig_vsd) = c("shrh_siC","tham_siC","zhou_siC",
+                "shrh_siK","tham_siK","zhou_siK")
+  colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(20))
+  heatmap.2(sig_vsd,col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+  xlab="", ylab="E2F Target Genes",key.title="Gene expression",cexCol=.65,cexRow=.6)
+dev.off()
+
+pdf("dreamComplex_all.pdf")
+sig_vsd = vsd[rownames(vsd) %in% c("LIN9","LIN54","LIN52","LIN37","FOXM1","MYB","MYBL2","KCNIP3","RB1","KIF2A","E2F2",
+                                    "DYRK1A","TP53","CDKN1A","E2F4","RBBP4","TFDP1","RBL2","RBL1","E2F5","TFDP2","MYBL1",
+                                    "NOLC1","FLNA","BIRC5","PLK1","CDC25C","CLTC","LATS2","SPTA1","MEIS2","SPTBN1","CLTA",
+                                    "FAM111A","NEIL3","E2F1","E2F3","E2F6","E2F7","E2F8"),]
+sig_vsd = sig_vsd[complete.cases(sig_vsd),]
+for(ix in 1:dim(sig_vsd)[1]){
+  sig_vsd[ix,1] = sig_vsd[ix,1]/mean(sig_vsd[ix,c(1,4)])
+  sig_vsd[ix,4] = sig_vsd[ix,4]/mean(sig_vsd[ix,c(1,4)])
+  sig_vsd[ix,2] = sig_vsd[ix,2]/mean(sig_vsd[ix,c(2,5)])
+  sig_vsd[ix,5] = sig_vsd[ix,5]/mean(sig_vsd[ix,c(2,5)])
+  sig_vsd[ix,3] = sig_vsd[ix,3]/mean(sig_vsd[ix,c(3,6)])
+  sig_vsd[ix,6] = sig_vsd[ix,6]/mean(sig_vsd[ix,c(3,6)])
+  }
+
+  colnames(sig_vsd) = c("shrh_siC","tham_siC","zhou_siC",
+                "shrh_siK","tham_siK","zhou_siK")
+  colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(20))
+  heatmap.2(sig_vsd,col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+  xlab="", ylab="DREAM complex Genes",key.title="Gene expression",cexCol=.65,cexRow=.6)
+dev.off()
+
+pdf("dreamComplex_significant.pdf")
+sig_vsd = vsd[rownames(vsd) %in% c("LIN9","LIN54","LIN52","LIN37","FOXM1","MYB","MYBL2","KCNIP3","RB1","KIF2A","E2F2",
+                                    "DYRK1A","TP53","CDKN1A","E2F4","RBBP4","TFDP1","RBL2","RBL1","E2F5","TFDP2","MYBL1",
+                                    "NOLC1","FLNA","BIRC5","PLK1","CDC25C","CLTC","LATS2","SPTA1","MEIS2","SPTBN1","CLTA",
+                                    "FAM111A","NEIL3","E2F1","E2F3","E2F6","E2F7","E2F8"),]
+sig_vsd = sig_vsd[complete.cases(sig_vsd),]
+sig_vsd = sig_vsd[rownames(sig_vsd) %in% rownames(dLRT_res)[which(abs(dLRT_res$log2FoldChange)>1 & dLRT_res$padj<0.05)], ]
+for(ix in 1:dim(sig_vsd)[1]){
+  sig_vsd[ix,1] = sig_vsd[ix,1]/mean(sig_vsd[ix,c(1,4)])
+  sig_vsd[ix,4] = sig_vsd[ix,4]/mean(sig_vsd[ix,c(1,4)])
+  sig_vsd[ix,2] = sig_vsd[ix,2]/mean(sig_vsd[ix,c(2,5)])
+  sig_vsd[ix,5] = sig_vsd[ix,5]/mean(sig_vsd[ix,c(2,5)])
+  sig_vsd[ix,3] = sig_vsd[ix,3]/mean(sig_vsd[ix,c(3,6)])
+  sig_vsd[ix,6] = sig_vsd[ix,6]/mean(sig_vsd[ix,c(3,6)])
+  }
+
+  colnames(sig_vsd) = c("shrh_siC","tham_siC","zhou_siC",
+                "shrh_siK","tham_siK","zhou_siK")
+  colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(20))
+  heatmap.2(sig_vsd,col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+  xlab="", ylab="DREAM complex Genes",key.title="Gene expression",cexCol=.65,cexRow=.6)
+dev.off()
+####################################################################################################################################
