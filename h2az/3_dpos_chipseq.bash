140a02b871bb47995690611c5b11dd34e2298fdd
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-09-11 14:56:16 +0800

    Update 3_dpos_chipseq.bash

--- h2az/3_dpos_chipseq.bash
+++ h2az/3_dpos_chipseq.bash
@@ -1,4 +1,4 @@
-python /home/rtm/myprograms/danpos-2.2.2/danpos.py dpos -o siControl h2az_siControl_rmdup.bam:input_siControl_rmdup.bam &> siControl.log &
-python /home/rtm/myprograms/danpos-2.2.2/danpos.py dpos -o siTIP60 h2az_siTIP60_rmdup.bam:input_siTIP60_rmdup.bam &> siTIP60.log &
+python /home/rtm/myprograms/danpos-2.2.2/danpos.py dpos --bg  -o siControl h2az_siControl_rmdup.bam:input_siControl_rmdup.bam &> siControl.log &
+python /home/rtm/myprograms/danpos-2.2.2/danpos.py dpos --bg  -o siTIP60 h2az_siTIP60_rmdup.bam:input_siTIP60_rmdup.bam &> siTIP60.log &
 
 python /home/rtm/myprograms/danpos-2.2.2/danpos.py wiq --buffer_size 70 hg38.chrom.sizes_noCHR NTC.smooth.wig:siMyc.smooth.wig &> norm.log
