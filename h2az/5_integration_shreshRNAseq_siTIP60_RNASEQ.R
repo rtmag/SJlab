ae581d119d563e64039630161f702d7ab008b940
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-09-14 16:07:09 +0800

    Update 5_integration_shreshRNAseq_siTIP60_RNASEQ.R

--- h2az/5_integration_shreshRNAseq_siTIP60_RNASEQ.R
+++ h2az/5_integration_shreshRNAseq_siTIP60_RNASEQ.R
@@ -7,4 +7,4 @@
 write.table(rownames(h2_dw[rownames(h2_dw) %in% k5_dw[,1],]),"h2_dw_k5_dw",sep="\t",quote=F,col.names=F,row.names=F)
 write.table(rownames(h2_dw[rownames(h2_dw) %in% k5_up[,1],]),"h2_dw_k5_up",sep="\t",quote=F,col.names=F,row.names=F)
 write.table(rownames(h2_up[rownames(h2_up) %in% k5_dw[,1],]),"h2_up_k5_dw",sep="\t",quote=F,col.names=F,row.names=F)
-write.table(rownames(h2_up[rownames(h2_up) %in% k5_up[,1],]),"h2_dw_k5_dw",sep="\t",quote=F,col.names=F,row.names=F)
+write.table(rownames(h2_up[rownames(h2_up) %in% k5_up[,1],]),"h2_up_k5_up",sep="\t",quote=F,col.names=F,row.names=F)
