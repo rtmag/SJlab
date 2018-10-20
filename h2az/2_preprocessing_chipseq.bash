c7f23fecf01d76ccc250d7c762172741c80879ff
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-10-21 01:16:29 +0200

    Update 2_preprocessing_chipseq.bash

--- h2az/2_preprocessing_chipseq.bash
+++ h2az/2_preprocessing_chipseq.bash
@@ -118,5 +118,17 @@ diffReps.pl --treatment ach2az_siTIP60_rmdup.bed --btr input_siTIP60_rmdup.bed \
 --control ach2az_siControl_rmdup.bed --bco input_siControl_rmdup.bed \
 --nohs --noanno --nsd 20 --chrlen /home/roberto/references/hg38.chrom.sizes \
 --meth gt --report ach2az_sik_vs_siC --frag 100 --nproc 60 --window 100 --pval 0.0001
+##################################################################################################
+bamCoverage -p max -bs 1 --normalizeUsing CPM -b /home/roberto/h2az/bam_chip/h2az_siControl_rmdup.bam \
+-o /home/roberto/h2az/bw/h2az_siControl.bw
+
+bamCoverage -p max -bs 1 --normalizeUsing CPM -b /home/roberto/h2az/bam_chip/h2az_siTIP60_rmdup.bam \
+-o /home/roberto/h2az/bw/h2az_siTIP60.bw
+
+bamCoverage -p max -bs 1 --normalizeUsing CPM -b /home/roberto/h2az/bam_chip/ach2az_siControl_rmdup.bam \
+-o /home/roberto/h2az/bw/ach2az_siControl.bw
+
+bamCoverage -p max -bs 1 --normalizeUsing CPM -b /home/roberto/h2az/bam_chip/ach2az_siTIP60_rmdup.bam \
+-o /home/roberto/h2az/bw/ach2az_siTIP60.bw
 #################################################################################################
 annotatePeaks.pl ach2az_sik_vs_siC_down_123.bed hg38 -annStats ach2az_sik_vs_siC_down_123.annStats > ach2az_sik_vs_siC_down_123.bed.anno
