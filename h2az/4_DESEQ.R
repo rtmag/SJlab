fee4795d3198fb4ab382c086ceac344e8ba8cb2f
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-10-21 04:15:07 +0200

    Update 4_DESEQ.R

--- h2az/4_DESEQ.R
+++ h2az/4_DESEQ.R
@@ -104,14 +104,16 @@ pdf("e2f_targets_siginificantChange.pdf")
 sig_vsd = vsd[(rownames(vsd) %in% gmt[,1]) & ((abs(dLRT_res$log2FoldChange)>1 & dLRT_res$padj<0.05)),]
 sig_vsd = sig_vsd[complete.cases(sig_vsd),]
 sig_vsd = sig_vsd[complete.cases(sig_vsd),]
+sig_vsd_tmp = sig_vsd
 for(ix in 1:dim(sig_vsd)[1]){
-  sig_vsd[ix,1] = sig_vsd[ix,1]-mean(sig_vsd[ix,c(1,4)])
-  sig_vsd[ix,4] = sig_vsd[ix,4]-mean(sig_vsd[ix,c(1,4)])
-  sig_vsd[ix,2] = sig_vsd[ix,2]-mean(sig_vsd[ix,c(2,5)])
-  sig_vsd[ix,5] = sig_vsd[ix,5]-mean(sig_vsd[ix,c(2,5)])
-  sig_vsd[ix,3] = sig_vsd[ix,3]-mean(sig_vsd[ix,c(3,6)])
-  sig_vsd[ix,6] = sig_vsd[ix,6]-mean(sig_vsd[ix,c(3,6)])
+  sig_vsd_tmp[ix,1] = sig_vsd[ix,1]-mean(sig_vsd[ix,c(1,4)])
+  sig_vsd_tmp[ix,4] = sig_vsd[ix,4]-mean(sig_vsd[ix,c(1,4)])
+  sig_vsd_tmp[ix,2] = sig_vsd[ix,2]-mean(sig_vsd[ix,c(2,5)])
+  sig_vsd_tmp[ix,5] = sig_vsd[ix,5]-mean(sig_vsd[ix,c(2,5)])
+  sig_vsd_tmp[ix,3] = sig_vsd[ix,3]-mean(sig_vsd[ix,c(3,6)])
+  sig_vsd_tmp[ix,6] = sig_vsd[ix,6]-mean(sig_vsd[ix,c(3,6)])
   }
+sig_vsd = sig_vsd_tmp
 
   colnames(sig_vsd) = c("shrh_siC","tham_siC","zhou_siC",
                 "shrh_siK","tham_siK","zhou_siK")
@@ -124,14 +126,16 @@ pdf("e2f_targets_ALL.pdf")
 sig_vsd = vsd[(rownames(vsd) %in% gmt[,1]),]
 sig_vsd = sig_vsd[complete.cases(sig_vsd),]
 sig_vsd = sig_vsd[complete.cases(sig_vsd),]
+sig_vsd_tmp = sig_vsd
 for(ix in 1:dim(sig_vsd)[1]){
-  sig_vsd[ix,1] = sig_vsd[ix,1]-mean(sig_vsd[ix,c(1,4)])
-  sig_vsd[ix,4] = sig_vsd[ix,4]-mean(sig_vsd[ix,c(1,4)])
-  sig_vsd[ix,2] = sig_vsd[ix,2]-mean(sig_vsd[ix,c(2,5)])
-  sig_vsd[ix,5] = sig_vsd[ix,5]-mean(sig_vsd[ix,c(2,5)])
-  sig_vsd[ix,3] = sig_vsd[ix,3]-mean(sig_vsd[ix,c(3,6)])
-  sig_vsd[ix,6] = sig_vsd[ix,6]-mean(sig_vsd[ix,c(3,6)])
+  sig_vsd_tmp[ix,1] = sig_vsd[ix,1]-mean(sig_vsd[ix,c(1,4)])
+  sig_vsd_tmp[ix,4] = sig_vsd[ix,4]-mean(sig_vsd[ix,c(1,4)])
+  sig_vsd_tmp[ix,2] = sig_vsd[ix,2]-mean(sig_vsd[ix,c(2,5)])
+  sig_vsd_tmp[ix,5] = sig_vsd[ix,5]-mean(sig_vsd[ix,c(2,5)])
+  sig_vsd_tmp[ix,3] = sig_vsd[ix,3]-mean(sig_vsd[ix,c(3,6)])
+  sig_vsd_tmp[ix,6] = sig_vsd[ix,6]-mean(sig_vsd[ix,c(3,6)])
   }
+sig_vsd = sig_vsd_tmp
 
   colnames(sig_vsd) = c("shrh_siC","tham_siC","zhou_siC",
                 "shrh_siK","tham_siK","zhou_siK")
@@ -146,14 +150,16 @@ sig_vsd = vsd[rownames(vsd) %in% c("LIN9","LIN54","LIN52","LIN37","FOXM1","MYB",
                                     "NOLC1","FLNA","BIRC5","PLK1","CDC25C","CLTC","LATS2","SPTA1","MEIS2","SPTBN1","CLTA",
                                     "FAM111A","NEIL3","E2F1","E2F3","E2F6","E2F7","E2F8"),]
 sig_vsd = sig_vsd[complete.cases(sig_vsd),]
+sig_vsd_tmp = sig_vsd
 for(ix in 1:dim(sig_vsd)[1]){
-  sig_vsd[ix,1] = sig_vsd[ix,1]-mean(sig_vsd[ix,c(1,4)])
-  sig_vsd[ix,4] = sig_vsd[ix,4]-mean(sig_vsd[ix,c(1,4)])
-  sig_vsd[ix,2] = sig_vsd[ix,2]-mean(sig_vsd[ix,c(2,5)])
-  sig_vsd[ix,5] = sig_vsd[ix,5]-mean(sig_vsd[ix,c(2,5)])
-  sig_vsd[ix,3] = sig_vsd[ix,3]-mean(sig_vsd[ix,c(3,6)])
-  sig_vsd[ix,6] = sig_vsd[ix,6]-mean(sig_vsd[ix,c(3,6)])
+  sig_vsd_tmp[ix,1] = sig_vsd[ix,1]-mean(sig_vsd[ix,c(1,4)])
+  sig_vsd_tmp[ix,4] = sig_vsd[ix,4]-mean(sig_vsd[ix,c(1,4)])
+  sig_vsd_tmp[ix,2] = sig_vsd[ix,2]-mean(sig_vsd[ix,c(2,5)])
+  sig_vsd_tmp[ix,5] = sig_vsd[ix,5]-mean(sig_vsd[ix,c(2,5)])
+  sig_vsd_tmp[ix,3] = sig_vsd[ix,3]-mean(sig_vsd[ix,c(3,6)])
+  sig_vsd_tmp[ix,6] = sig_vsd[ix,6]-mean(sig_vsd[ix,c(3,6)])
   }
+sig_vsd = sig_vsd_tmp
 
   colnames(sig_vsd) = c("shrh_siC","tham_siC","zhou_siC",
                 "shrh_siK","tham_siK","zhou_siK")
@@ -169,14 +175,16 @@ sig_vsd = vsd[rownames(vsd) %in% c("LIN9","LIN54","LIN52","LIN37","FOXM1","MYB",
                                     "FAM111A","NEIL3","E2F1","E2F3","E2F6","E2F7","E2F8"),]
 sig_vsd = sig_vsd[complete.cases(sig_vsd),]
 sig_vsd = sig_vsd[rownames(sig_vsd) %in% rownames(dLRT_res)[which(abs(dLRT_res$log2FoldChange)>1 & dLRT_res$padj<0.05)], ]
+sig_vsd_tmp = sig_vsd
 for(ix in 1:dim(sig_vsd)[1]){
-  sig_vsd[ix,1] = sig_vsd[ix,1]-mean(sig_vsd[ix,c(1,4)])
-  sig_vsd[ix,4] = sig_vsd[ix,4]-mean(sig_vsd[ix,c(1,4)])
-  sig_vsd[ix,2] = sig_vsd[ix,2]-mean(sig_vsd[ix,c(2,5)])
-  sig_vsd[ix,5] = sig_vsd[ix,5]-mean(sig_vsd[ix,c(2,5)])
-  sig_vsd[ix,3] = sig_vsd[ix,3]-mean(sig_vsd[ix,c(3,6)])
-  sig_vsd[ix,6] = sig_vsd[ix,6]-mean(sig_vsd[ix,c(3,6)])
+  sig_vsd_tmp[ix,1] = sig_vsd[ix,1]-mean(sig_vsd[ix,c(1,4)])
+  sig_vsd_tmp[ix,4] = sig_vsd[ix,4]-mean(sig_vsd[ix,c(1,4)])
+  sig_vsd_tmp[ix,2] = sig_vsd[ix,2]-mean(sig_vsd[ix,c(2,5)])
+  sig_vsd_tmp[ix,5] = sig_vsd[ix,5]-mean(sig_vsd[ix,c(2,5)])
+  sig_vsd_tmp[ix,3] = sig_vsd[ix,3]-mean(sig_vsd[ix,c(3,6)])
+  sig_vsd_tmp[ix,6] = sig_vsd[ix,6]-mean(sig_vsd[ix,c(3,6)])
   }
+sig_vsd = sig_vsd_tmp
 
   colnames(sig_vsd) = c("shrh_siC","tham_siC","zhou_siC",
                 "shrh_siK","tham_siK","zhou_siK")
