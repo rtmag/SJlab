105e2a4ef82abf936ba87b321158bb207652dfe4
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-05-10 04:12:08 +0800

    Update 2_preprocessing_chipseq.bash

--- h2az/2_preprocessing_chipseq.bash
+++ h2az/2_preprocessing_chipseq.bash
@@ -113,11 +113,18 @@ bamToBed -i ach2az_siTIP60_rmdup.bam > ../bed/ach2az_siTIP60_rmdup.bed &
 bamToBed -i input_siControl_rmdup.bam > ../bed/input_siControl_rmdup.bed &
 bamToBed -i input_siTIP60_rmdup.bam > ../bed/input_siTIP60_rmdup.bed &
 
+bamToBed -i h2az_siControl_rmdup.bam > ../bed/h2az_siControl_rmdup.bed &
+bamToBed -i h2az_siTIP60_rmdup.bam > ../bed/h2az_siTIP60_rmdup.bed &
 
 diffReps.pl --treatment ach2az_siTIP60_rmdup.bed --btr input_siTIP60_rmdup.bed \
 --control ach2az_siControl_rmdup.bed --bco input_siControl_rmdup.bed \
 --nohs --noanno --nsd 20 --chrlen /home/roberto/references/hg38.chrom.sizes \
 --meth gt --report ach2az_sik_vs_siC --frag 100 --nproc 60 --window 100 --pval 0.0001
+
+diffReps.pl --treatment h2az_siTIP60_rmdup.bed --btr input_siTIP60_rmdup.bed \
+--control h2az_siControl_rmdup.bed --bco input_siControl_rmdup.bed \
+--nohs --noanno --nsd 20 --chrlen /home/roberto/references/hg38.chrom.sizes \
+--meth gt --report h2az_sik_vs_siC --frag 100 --nproc 60 --window 100 --pval 0.0001
 ##################################################################################################
 samtools index h2az_siControl_rmdup.bam &
 samtools index h2az_siTIP60_rmdup.bam &
@@ -137,3 +144,28 @@ bamCoverage -p max -bs 1 --normalizeUsing CPM -b /home/roberto/h2az/bam_chip/ach
 -o /home/roberto/h2az/bw/ach2az_siTIP60.bw &
 #################################################################################################
 annotatePeaks.pl ach2az_sik_vs_siC_down_123.bed hg38 -annStats ach2az_sik_vs_siC_down_123.annStats > ach2az_sik_vs_siC_down_123.bed.anno
+#################################################################################################
+macs2 callpeak -g hs -q 0.05 --keep-dup auto --broad -n ach2az_siControl_broad --outdir /home/roberto/h2az/macs2 \
+-t /home/roberto/h2az/bam_chip/ach2az_siControl_rmdup.bam -c /home/roberto/h2az/bam_chip/input_siControl_rmdup.bam &
+
+macs2 callpeak -g hs -q 0.05 --keep-dup auto --broad -n ach2az_siTIP60_broad --outdir /home/roberto/h2az/macs2 \
+-t /home/roberto/h2az/bam_chip/ach2az_siTIP60_rmdup.bam -c /home/roberto/h2az/bam_chip/input_siTIP60_rmdup.bam &
+
+macs2 callpeak -g hs -q 0.05 --keep-dup auto --broad -n h2az_siControl_broad --outdir /home/roberto/h2az/macs2 \
+-t /home/roberto/h2az/bam_chip/h2az_siControl_rmdup.bam -c /home/roberto/h2az/bam_chip/input_siControl_rmdup.bam &
+
+macs2 callpeak -g hs -q 0.05 --keep-dup auto --broad -n h2az_siTIP60_broad --outdir /home/roberto/h2az/macs2 \
+-t /home/roberto/h2az/bam_chip/h2az_siTIP60_rmdup.bam -c /home/roberto/h2az/bam_chip/input_siTIP60_rmdup.bam &
+##################################################################################################################
+macs2 callpeak -g hs -q 0.05 --keep-dup auto --call-summits -n ach2az_siControl_narrow --outdir /home/roberto/h2az/macs2 \
+-t /home/roberto/h2az/bam_chip/ach2az_siControl_rmdup.bam -c /home/roberto/h2az/bam_chip/input_siControl_rmdup.bam &
+
+macs2 callpeak -g hs -q 0.05 --keep-dup auto --call-summits -n ach2az_siTIP60_narrow --outdir /home/roberto/h2az/macs2 \
+-t /home/roberto/h2az/bam_chip/ach2az_siTIP60_rmdup.bam -c /home/roberto/h2az/bam_chip/input_siTIP60_rmdup.bam &
+
+macs2 callpeak -g hs -q 0.05 --keep-dup auto --call-summits -n h2az_siControl_narrow --outdir /home/roberto/h2az/macs2 \
+-t /home/roberto/h2az/bam_chip/h2az_siControl_rmdup.bam -c /home/roberto/h2az/bam_chip/input_siControl_rmdup.bam &
+
+macs2 callpeak -g hs -q 0.05 --keep-dup auto --call-summits -n h2az_siTIP60_narrow --outdir /home/roberto/h2az/macs2 \
+-t /home/roberto/h2az/bam_chip/h2az_siTIP60_rmdup.bam -c /home/roberto/h2az/bam_chip/input_siTIP60_rmdup.bam &
+#################################################################################################
