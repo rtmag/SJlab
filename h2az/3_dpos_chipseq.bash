9f4ee9716cf441dde3dcac505c0d9ccfdd01189b
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-05-10 18:04:06 +0800

    Update 3_dpos_chipseq.bash

--- h2az/3_dpos_chipseq.bash
+++ h2az/3_dpos_chipseq.bash
@@ -9,4 +9,19 @@ python /home/rtm/myprograms/danpos-2.2.2/danpos.py wiq --buffer_size 70 /home/rt
 
 /home/rtm/myprograms/kentUtils/bin/linux.x86_64/wigToBigWig home_rtm_CSI_sjlab_h2az_siTIP60_pooled_h2az_siTIP60_rmdup.smooth.qnor.wig \
 /home/rtm/references/hg38.chrom.sizes h2az_siTIP60_rmdup.smooth.qnor.bw -clip &
-######
+####################################################################################################################################
+python /home/rtm/myprograms/danpos-2.2.2/danpos.py dpos -o siControl -n N ach2az_siControl_rmdup.bam &> siControl.log &
+python /home/rtm/myprograms/danpos-2.2.2/danpos.py dpos -o siTIP60 -n N ach2az_siTIP60_rmdup.bam &> siTIP60.log &
+
+wait
+
+python /home/rtm/myprograms/danpos-2.2.2/danpos.py wiq --buffer_size 70 /home/rtm/references/hg38.chrom.sizes \
+/home/rtm/CSI/sjlab/h2az/ach2az/siControl/pooled/ach2az_siControl_rmdup.smooth.wig:/home/rtm/CSI/sjlab/h2az/ach2az/siTIP60/pooled/ach2az_siTIP60_rmdup.smooth.wig &> norm.log
+
+##########
+
+/home/rtm/myprograms/kentUtils/bin/linux.x86_64/wigToBigWig home_rtm_CSI_sjlab_h2az_siControl_pooled_h2az_siControl_rmdup.smooth.qnor.wig \
+/home/rtm/references/hg38.chrom.sizes h2az_siControl_rmdup.smooth.qnor.bw -clip &
+
+/home/rtm/myprograms/kentUtils/bin/linux.x86_64/wigToBigWig home_rtm_CSI_sjlab_h2az_siTIP60_pooled_h2az_siTIP60_rmdup.smooth.qnor.wig \
+/home/rtm/references/hg38.chrom.sizes h2az_siTIP60_rmdup.smooth.qnor.bw -clip &
