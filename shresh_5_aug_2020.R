cdebf2d6157dad6bf7acf7193042f151b152c944
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-08-05 23:01:18 +0800

    Update shresh_5_aug_2020.R

--- shresh_5_aug_2020.R
+++ shresh_5_aug_2020.R
@@ -35,3 +35,22 @@ pdf("test.pdf",width=77)
 barplot(data_patients_numeric_sorted,las=2,ylab="Tumor - Normal", col=color_vector ,xlab = "1.3K BRCA samples")
 abline(h=0)
 dev.off()
+
+##########################################################################################################################################################################
+# jitter plot 
+library(ggplot2)
+
+data_patients <- data[ 2, 11:dim(data)[2] ]
+col_jit <- colnames(data_patients)
+col_jit[grep("Norm",col_jit)] <- "Normal"
+col_jit[grep("Norm",col_jit,invert=T)] <- "Tumor"
+data_patients_numeric <- as.numeric(as.matrix(data_patients))
+
+data_box <- data.frame(sample = col_jit, value = data_patients_numeric)
+
+pdf("jitterplot.pdf")
+stripchart(value ~ sample, vertical = TRUE, data = data_box, jitter = 0.3, ylab = expression('ITAG9 exon skipping'),
+    method = "jitter", pch = 20, col = alpha(colour='red',alpha=.5),cex = 2)
+boxplot(value ~ sample, data = data_box,add=T,lwd=4)
+dev.off()
+
